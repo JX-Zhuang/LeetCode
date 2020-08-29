42af5e5241b96d19b634ecb4e6a52ac62dd64382
JX-Zhuang <who.zhuang@gmail.com>
2020-08-30 00:26:45 +0800

    update

--- 树/117. 填充每个节点的下一个右侧节点指针 II.js
+++ 树/117. 填充每个节点的下一个右侧节点指针 II.js
@@ -15,28 +15,25 @@
 var connect = function(root) {
     if(!root) return null;
     var leftmost = root;
-    while(leftmost.left || leftmost.right){
-        var head = leftmost;
-        while(head){
-            if(head.left){
-                var leftNext = null;
-                if(head.right){
-                    leftNext = head.right;
-                }else if(head.next){
-                    leftNext = head.next.left || head.next.right;
-                }
-                head.left.next = leftNext;
-            }
-            if(head.right){
-                var rightNext = null;
-                if(head.next){
-                    rightNext = head.next.left || head.next.right;
-                }
-                head.right.next = rightNext;
-            }
-            head = head.next;
+    var current,prev;
+    var processChild = function(node){
+        if(!node) return;
+        if(prev){
+            prev.next = node;
+        }else{
+            leftmost = node;
         }
-        leftmost = leftmost.left || leftmost.right;
+        prev = node;
+    };
+    while(leftmost){
+        prev = null;
+        current = leftmost;
+        leftmost = null;
+        while(current){
+            processChild(current.left);
+            processChild(current.right);
+            current = current.next;
+        }
     }
     return root;
 };
