f59403c9efb4c4a4d65b59afa5f342414672183a
JX-Zhuang <who.zhuang@gmail.com>
2020-09-10 23:50:36 +0800

    widthOfBinaryTree

---
+++ 树/662. 二叉树最大宽度.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function (root) {
+    var res = 0;
+    var o = {};
+    var dfs = function (node, depth, pos) {
+        if (!node) return;
+        if (o[depth] === undefined) {
+            o[depth] = pos;
+        }
+        var n = pos - o[depth] + 1;
+        if (n > res) {
+            res = n;
+        }
+        dfs(node.left, depth + 1n, pos * 2n);
+        dfs(node.right, depth + 1n, pos * 2n + 1);
+    };
+    dfs(root, 0n, 0n);
+    return res;
+};
