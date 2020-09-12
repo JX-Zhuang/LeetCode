93ebd033a871951d2aaf72c1f238e9ff6c6d7a88
JX-Zhuang <who.zhuang@gmail.com>
2020-09-12 23:57:52 +0800

    gardenNoAdj

--- 图/1042. 不邻接植花.js
+++ 图/1042. 不邻接植花.js
@@ -6,11 +6,11 @@
 var gardenNoAdj = function (N, paths) {
     var res = new Array(N).fill(0);
     var graph = [];
-    for (var i = 1; i <= N; i++) {
+    for(var i = 1;i<=N;i++){
         graph[i] = [];
     }
-    for (var path of paths) {
-        var g1 = path[0], g2 = path[1];
+    for(var path of paths){
+        var g1 = path[0],g2 = path[1];
         graph[g1].push(g2);
         graph[g2].push(g1);
     }
@@ -19,8 +19,8 @@ var gardenNoAdj = function (N, paths) {
         var gardens = graph[i];
         for (var j = 0; j < gardens.length; j++) {
             var g = gardens[j];
-            var flowerIndex = flowers.indexOf(res[g - 1]);
-            if (flowerIndex != -1) {
+            var flowerIndex = flowers.indexOf(res[g-1]);
+            if (flowerIndex !== -1) {
                 flowers.splice(flowerIndex, 1);
             }
         }
