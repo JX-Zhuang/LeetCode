211472891db8f4604723f193b2d9ad6bf76d02c1
JX-Zhuang <who.zhuang@gmail.com>
2020-09-15 23:24:02 +0800

    findRedundantConnection

--- 树/684. 冗余连接.js
+++ 树/684. 冗余连接.js
@@ -2,26 +2,56 @@
  * @param {number[][]} edges
  * @return {number[]}
  */
-var findRedundantConnection = function(edges) {
-    var res;
-    var paths = {};
-    for(var edge of edges){
-        var e1 = edge[0],e2 = edge[1];
-        if(!paths.hasOwnProperty(e1)){
-            paths[e1] = 0;
+var findRedundantConnection = function (edges) {
+    var graph = {};
+    var visited = [];
+    var l = edges.length;
+    var helper = function (edge) {
+        var e0 = edge[0], e1 = edge[1];
+        visited[e0] = 1;
+        for (var i = 0; i < graph[e0].length; i++) {
+            if (graph[e0][i] === e1) continue;
+            if (dfs(graph[e0][i], e1)) {
+                visited[e0];
+                return true;
+            }
         }
-        if(!paths.hasOwnProperty(e2)){
-            paths[e2] = 0;
+        visited[e0] = 0;
+        return false;
+    }
+    var dfs = function (start, end) {
+        visited[start] = 1;
+        for (var i = 0; i < graph[start].length; i++) {
+            var node = graph[start][i];
+            if (visited[node] === 1) continue;
+            if (node === end) {
+                return true;
+            } else {
+                if (dfs(node, end)) {
+                    return true;
+                }
+            }
         }
-        paths[e1]++;
-        paths[e2]++;
+        visited[start] = 0;
+        return false;
     }
-    console.log(paths)
-    for(var edge of edges){
-        var e1 = edge[0],e2 = edge[1];
-        if(paths[e1]>1&&paths[e2]>1) res = edge;
+    for (var edge of edges) {
+        var e1 = edge[0], e2 = edge[1];
+        if (!graph.hasOwnProperty(e1)) {
+            graph[e1] = [];
+        }
+        if (!graph.hasOwnProperty(e2)) {
+            graph[e2] = [];
+        }
+        graph[e1].push(e2);
+        graph[e2].push(e1);
     }
-    return res;
+    for (var i = l - 1; i >= 0; i--) {
+        if (helper(edges[i])) {
+            return edges[i];
+        }
+    }
+    return [];
 };
 //   4
 //   | \
