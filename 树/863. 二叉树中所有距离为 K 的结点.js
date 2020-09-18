ee154ab5b1f72cefce8771fe6051defb62ad4757
JX-Zhuang <who.zhuang@gmail.com>
2020-09-18 23:39:59 +0800

    distanceK

--- 树/863. 二叉树中所有距离为 K 的结点.js
+++ 树/863. 二叉树中所有距离为 K 的结点.js
@@ -11,6 +11,32 @@
  * @param {number} K
  * @return {number[]}
  */
-var distanceK = function (root, target, K) {
-
+var distanceK = function(root, target, K) {
+    var l = 0;
+    var getLevel = function(node,level){
+        if(!node) return;
+        if(node.val === target.val){
+            l = level;
+            return ;
+        }
+        getLevel(node.left,level+1);
+        getLevel(node.right,level+1);
+    }
+    getLevel(root,0);
+    var res = [];
+    var dfs = function(node,level){
+        if(!node) return;
+        if(level === l + K ){
+            res.push(node.val);
+        }
+        dfs(node.left,level+1);
+        dfs(node.right,level+1);
+    }
+    dfs(root,0);
+    return res;
 };
+
+    //    3
+    //  5   1
+    // 6 2 0 8
+    //  7 4 
