97d3deb848570b55abebf6a273da1a55d45b1a09
JX-Zhuang <who.zhuang@gmail.com>
2020-09-21 23:33:49 +0800

    constructFromPrePost

---
+++ 树/889. 根据前序和后序遍历构造二叉树.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} pre
+ * @param {number[]} post
+ * @return {TreeNode}
+ */
+var constructFromPrePost = function (pre, post) {
+    var o = {};
+    var l = post.length;
+    for (var i = 0; i < l; i++) {
+        o[post[i]] = i;
+    }
+    var buildTree = function (leftPre, rightPre, leftPost, rightPost) {
+        if (leftPre + 1 >= l) return null;
+        if (leftPre > rightPre || leftPost > rightPost) return null;
+        var rootVal = pre[leftPre];
+        var root = new TreeNode(rootVal);
+        var val = pre[leftPre + 1];
+        var index = o[val];
+        var newRightPre = index - leftPost + 1 + leftPre;
+        root.left = buildTree(leftPre + 1, newRightPre, leftPost, index);
+        root.right = buildTree(newRightPre + 1, rightPre, index + 1, rightPost - 1);
+        return root;
+    };
+    return buildTree(0, l - 1, 0, l - 1);
+};
