20ca2e191f5b471de418dfab48243b5a4fab1f39
JX-Zhuang <who.zhuang@gmail.com>
2020-09-22 23:39:54 +0800

    CBTInserter

---
+++ 树/919. 完全二叉树插入器.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var CBTInserter = function(root) {
+
+};
+
+/** 
+ * @param {number} v
+ * @return {number}
+ */
+CBTInserter.prototype.insert = function(v) {
+
+};
+
+/**
+ * @return {TreeNode}
+ */
+CBTInserter.prototype.get_root = function() {
+
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * var obj = new CBTInserter(root)
+ * var param_1 = obj.insert(v)
+ * var param_2 = obj.get_root()
+ */
