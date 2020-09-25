993138e529a47d204d8588093455eecacbbd8423
JX-Zhuang <who.zhuang@gmail.com>
2020-09-25 23:52:38 +0800

    flipMatchVoyage

--- 树/971. 翻转二叉树以匹配先序遍历.js
+++ 树/971. 翻转二叉树以匹配先序遍历.js
@@ -11,5 +11,23 @@
  * @return {number[]}
  */
 var flipMatchVoyage = function(root, voyage) {
-
+    var arr = [];
+    var index = 0;
+    var dfs = function(node){
+        if(!node) return;
+        if(node.val!==voyage[index++]){
+            arr = [-1];
+            return;
+        }
+        if(index<voyage.length&&node.left&&node.left.val!==voyage[index]){
+            arr.push(node.val);
+            dfs(node.left);
+            dfs(node.right);
+        }else{
+            dfs(node.right);
+            dfs(node.left);
+        }
+    };
+    dfs(root);
+    return arr;
 };
