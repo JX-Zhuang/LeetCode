92faf038373880d7de269ca74e02a9f9f9e5ffef
JX-Zhuang <who.zhuang@gmail.com>
2020-10-01 23:41:50 +0800

    maxAncestorDiff

--- 树/1026. 节点与其祖先之间的最大差值.js
+++ 树/1026. 节点与其祖先之间的最大差值.js
@@ -9,6 +9,16 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxAncestorDiff = function(root) {
-
+var maxAncestorDiff = function (root) {
+    var result = 0;
+    var dfs = function (node, high, low) {
+        if (!node) return null;
+        result = Math.max(Math.max(Math.abs(node.val - high), Math.abs(node.val - low)), result);
+        high = Math.max(node.val, high);
+        low = Math.max(node.val, low);
+        dfs(node.left, high, low);
+        dfs(node.right, high, low);
+    };
+    dfs(root, root.val, root.val);
+    return result;
 };
