acfa8ffce92588d5fb564049fbd59ce22eeb3181
JX-Zhuang <who.zhuang@gmail.com>
2020-10-08 00:02:10 +0800

    mctFromLeafValues

--- 树/1130. 叶值的最小代价生成树.js
+++ 树/1130. 叶值的最小代价生成树.js
@@ -2,7 +2,15 @@
  * @param {number[]} arr
  * @return {number}
  */
-var mctFromLeafValues = function(arr) {
+var mctFromLeafValues = function (arr) {
+    var stack = [Number.MAX_SAFE_INTEGER];
     var res = 0;
+    for (var v of arr) {
+        while (stack.length >= 1 && stack[stack.length - 1] < v) {
+            res += stack.pop() * Math.min(stack[stack.length - 1], v);
+        }
+        stack.push(v);
+    }
+    while (stack.length > 2) res += stack.pop() * stack[stack.length - 1];
     return res;
 };
