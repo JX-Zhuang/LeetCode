30a45985a21d3e4cd4565d99ecfc72cb22795ba7
JX-Zhuang <who.zhuang@gmail.com>
2020-10-08 23:01:20 +0800

    btreeGameWinningMove

---
+++ 树/1145. 二叉树着色游戏.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} n
+ * @param {number} x
+ * @return {boolean}
+ */
+var btreeGameWinningMove = function (root, n, x) {
+
+};
