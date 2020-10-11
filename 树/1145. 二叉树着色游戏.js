2fcc014049c3ca1754099f8dcbf6d20aab242528
JX-Zhuang <who.zhuang@gmail.com>
2020-10-11 23:42:15 +0800

    btreeGameWinningMove

--- 树/1145. 二叉树着色游戏.js
+++ 树/1145. 二叉树着色游戏.js
@@ -12,4 +12,21 @@
  * @return {boolean}
  */
 var btreeGameWinningMove = function (root, n, x) {
+    var half = Math.floor(n / 2);
+    var left = 0, right = 0;
+    var dfs = function (node) {
+        var leftNum = 0, rightNum = 0;
+        if (!node) return 0;
+        leftNum = dfs(node.left);
+        rightNum = dfs(node.right);
+        if (node.val === x) {
+            left = leftNum;
+            right = rightNum;
+        }
+        return leftNum + rightNum + 1;
+    }
+    dfs(root);
+    if (left > half || right > half || (left + right) < half)
+        return true;
+    return false;
 };
