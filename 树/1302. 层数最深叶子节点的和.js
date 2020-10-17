bcafc79e93dd2f58592c52e204f86fbe9b03984a
JX-Zhuang <who.zhuang@gmail.com>
2020-10-17 22:53:25 +0800

    deepestLeavesSum

--- 树/1302. 层数最深叶子节点的和.js
+++ 树/1302. 层数最深叶子节点的和.js
@@ -9,6 +9,20 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var deepestLeavesSum = function(root) {
-
+var deepestLeavesSum = function (root) {
+    var maxLevel = 0;
+    var res = 0;
+    var dfs = function (node, level) {
+        if (!node) return;
+        if (level > maxLevel) {
+            maxLevel = level;
+            res = node.val;
+        } else if (maxLevel === level) {
+            res += node.val;
+        }
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
+    }
+    dfs(root, 1);
+    return res;
 };
