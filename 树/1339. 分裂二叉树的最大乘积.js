9d0da5bd1bf108467201aac2b7122bc0e86d62a1
JX-Zhuang <who.zhuang@gmail.com>
2020-10-26 22:48:15 +0800

    maxProduct

--- 树/1339. 分裂二叉树的最大乘积.js
+++ 树/1339. 分裂二叉树的最大乘积.js
@@ -10,4 +10,20 @@
  * @return {number}
  */
 var maxProduct = function(root) {
+	var getSum = function(node) {
+		if (!node) return 0;
+		return getSum(node.left) + getSum(node.right) + node.val;
+	};
+	var sum = getSum(root);
+	var result = 0;
+	var dfs = function(node) {
+		if (!node) return 0;
+		var current = dfs(node.left) + dfs(node.right) + node.val;
+		if (current * (sum - current) > result * (sum - result)) {
+			result = current;
+		}
+		return current;
+	};
+	dfs(root);
+	return (result * (sum - result)) % 1000000007;
 };
