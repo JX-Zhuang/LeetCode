bfd9b0fd8ebff729e4a43e67cdce83ccdca4e533
JX-Zhuang <who.zhuang@gmail.com>
2020-10-29 21:34:21 +0800

    longestZigZag

--- 树/1372. 二叉树中的最长交错路径.js
+++ 树/1372. 二叉树中的最长交错路径.js
@@ -10,4 +10,19 @@
  * @return {number}
  */
 var longestZigZag = function(root) {
+	var result = 0;
+	var dfs = function(node, pos, dis) {
+		if (!node) return;
+		result = Math.max(result, dis);
+		if (pos) {
+			dfs(node.left, 0, dis + 1);
+			dfs(node.right, 1, 1);
+		} else {
+			dfs(node.left, 0, 1);
+			dfs(node.right, 1, dis + 1);
+		}
+	};
+	dfs(root.left, 0, 1);
+	dfs(root.right, 1, 1);
+	return result;
 };
