a703fa43e922d7d92539c40a8f3f64e19a18620a
JX-Zhuang <who.zhuang@gmail.com>
2020-11-01 00:10:33 +0800

    minTime

--- 树/1443. 收集树上所有苹果的最少时间.js
+++ 树/1443. 收集树上所有苹果的最少时间.js
@@ -5,5 +5,28 @@
  * @return {number}
  */
 var minTime = function(n, edges, hasApple) {
-    
+	var map = {};
+	var visited = {};
+	for (var i = 0; i < n; i++) {
+		map[i] = [];
+	}
+	for (var e of edges) {
+		map[e[0]].push(e[1]);
+		map[e[1]].push(e[0]);
+	}
+	var helper = function(index) {
+		var selfOrChildHasApple = hasApple[index];
+		visited[index] = true;
+		for (var item of map[index]) {
+			if (visited[item]) continue;
+			selfOrChildHasApple = helper(item) || selfOrChildHasApple;
+		}
+		if (!selfOrChildHasApple) {
+			delete map[index];
+		}
+		return selfOrChildHasApple;
+	};
+	helper(0);
+	var size = Object.keys(map).length;
+	return size ? 2 * (size - 1) : 0;
 };
