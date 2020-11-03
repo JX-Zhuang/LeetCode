aca99ed3dc9c2c804eec30743bc69dd492e56645
JX-Zhuang <who.zhuang@gmail.com>
2020-11-03 23:50:59 +0800

    pseudoPalindromicPaths

--- 树/1457. 二叉树中的伪回文路径.js
+++ 树/1457. 二叉树中的伪回文路径.js
@@ -11,5 +11,19 @@
  * @return {number}
  */
 var pseudoPalindromicPaths  = function(root) {
-    
+    if (root == null) return 0;
+    var result = 0;
+    var dfs = function(node,temp){
+        temp ^= 1 << node.val;
+        if(!node.left&&!node.right){
+            if(temp==0||(temp&(temp-1))==0){
+                result++;
+            }
+            return;
+        }
+        node.left && dfs(node.left,temp);
+        node.right && dfs(node.right,temp);
+    };
+    dfs(root,0);
+    return result;
 };
