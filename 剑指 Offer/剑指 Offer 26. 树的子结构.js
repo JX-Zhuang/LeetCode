09ce9947daa13ea3944a4d17fe95f7c22829911c
JX-Zhuang <who.zhuang@gmail.com>
2020-11-08 23:55:54 +0800

    isSubStructure

---
+++ 剑指 Offer/剑指 Offer 26. 树的子结构.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} A
+ * @param {TreeNode} B
+ * @return {boolean}
+ */
+var isSubStructure = function(A, B) {
+
+};
