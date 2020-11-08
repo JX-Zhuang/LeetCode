1d8141299a5a4663f93a679cac965fce4264a653
JX-Zhuang <who.zhuang@gmail.com>
2020-11-08 23:19:08 +0800

    isEvenOddTree

--- 树/1609. 奇偶树.js
+++ 树/1609. 奇偶树.js
@@ -1,15 +1,30 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
 var isEvenOddTree = function(root) {
-    
+    var arr = [];
+    var dfs = function(node,level){
+        if(!node) return;
+        if(!arr[level]) arr[level] = [];
+        arr[level].push(node.val);
+        dfs(node.left,level+1);
+        dfs(node.right,level+1);
+    };
+    dfs(root,0);
+    for(var i = 0;i<arr.length;i++){
+        var item = arr[i];
+        if(item.length === 1){
+            if(i%2===0){
+                 if(item[0]%2===0) return false;
+            }else{
+                 if(item[0]%2===1) return false;
+            }
+            continue;
+        }
+        for(var j = 1;j<item.length;j++){
+            if(i%2 === 0){
+                if(item[j]%2===0 || item[j-1] >=item[j]) return false;
+            }else{
+                if(item[j]%2===1 || item[j-1] <=item[j]) return false;
+            }
+        }
+    }
+    return true;
 };
