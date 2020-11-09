ca261617a0d62bb9e9b44eef8eccf22d12b71d5d
JX-Zhuang <who.zhuang@gmail.com>
2020-11-09 23:33:18 +0800

    isSubStructure

--- 剑指 Offer/剑指 Offer 26. 树的子结构.js
+++ 剑指 Offer/剑指 Offer 26. 树的子结构.js
@@ -11,13 +11,10 @@
  * @return {boolean}
  */
 var isSubStructure = function (A, B) {
-    return (!(!A && B)) && !!B && (dfs(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    return A !== null && B !== null && (dfs(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
 };
 var dfs = function (A, B) {
     if (!B) return true;
-    if (!A) return false;
-    if (A.val === B.val) {
-        return dfs(A.left, B.left) && dfs(A.right, B.right);
-    }
-    return false;
+    if (!A || A.val !== B.val) return false;
+    return dfs(A.left, B.left) && dfs(A.right, B.right);
 }
