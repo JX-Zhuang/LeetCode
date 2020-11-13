b07af6263e7e12eb7ed699b9f91c32c3e55b5140
JX-Zhuang <who.zhuang@gmail.com>
2020-11-13 23:06:00 +0800

    inorderSuccessor

--- 程序员面试金典/面试题 04.06. 后继者.js
+++ 程序员面试金典/面试题 04.06. 后继者.js
@@ -11,5 +11,17 @@
  * @return {TreeNode}
  */
 var inorderSuccessor = function (root, p) {
-
+    var arr = [];
+    var dfs = function (node) {
+        if (!node) return;
+        dfs(node.left);
+        arr.push(node);
+        dfs(node.right);
+    };
+    dfs(root);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == p) {
+            return arr[i + 1] || null;
+        }
+    }
 };
