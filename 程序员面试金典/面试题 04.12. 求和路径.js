4bf5f75c2cc051750244575a556eb78b2cad0217
JX-Zhuang <who.zhuang@gmail.com>
2020-11-14 16:16:00 +0800

    pathSum

---
+++ 程序员面试金典/面试题 04.12. 求和路径.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+var pathSum = function (root, sum) {
+    var prevMap = { 0: 1 };
+    var dfs = function (node, prev) {
+        if (!node) return 0;
+        var count = 0;
+        prev += node.val;
+        if (prevMap.hasOwnProperty[prev - sum]) {
+            count += prevMap[prev - sum];
+        }
+        if (prevMap.hasOwnProperty(prev)) {
+            prevMap[prev]++;
+        } else {
+            prevMap[prev] = 1;
+        }
+        count += dfs(node.left, prev);
+        count += dfs(node.right, prev);
+        prevMap[prev]--;
+        return count;
+    }
+    return dfs(root, 0);
+};
