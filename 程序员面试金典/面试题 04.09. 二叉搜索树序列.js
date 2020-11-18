e7f8eb6e1d125e49838b86a5cac1edb17f19fe2d
JX-Zhuang <who.zhuang@gmail.com>
2020-11-18 23:34:38 +0800

    BSTSequences

--- 程序员面试金典/面试题 04.09. 二叉搜索树序列.js
+++ 程序员面试金典/面试题 04.09. 二叉搜索树序列.js
@@ -9,6 +9,24 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var BSTSequences = function(root) {
-
+var BSTSequences = function (root) {
+    if (!root) return [[]];
+    var result = [];
+    var dfs = function (node, q, path) {
+        if (node.left) {
+            q.push(node.left);
+        }
+        if (node.right) {
+            q.push(node.right);
+        }
+        if (q.length === 0) {
+            result.push(path);
+            return;
+        }
+        for (var n of q) {
+            dfs(n, q.filter(item => item != n), path.concat(n.val));
+        }
+    };
+    dfs(root, [], [root.val]);
+    return result;
 };
