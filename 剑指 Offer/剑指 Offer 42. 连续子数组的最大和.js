0af346ba5956abcaaaf72a0362912c74683f7776
JX-Zhuang <who.zhuang@gmail.com>
2020-12-01 23:55:37 +0800

    max sub arr

--- 剑指 Offer/剑指 Offer 42. 连续子数组的最大和.js
+++ 剑指 Offer/剑指 Offer 42. 连续子数组的最大和.js
@@ -2,5 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
+    var max = nums[0], sum = 0;
+    for (var n of nums) {
+        if (sum < 0) {
+            sum = 0;
+        } else {
+            sum += n;
+        }
+        max = Math.max(max, sum);
+    }
+    return max;
 };
