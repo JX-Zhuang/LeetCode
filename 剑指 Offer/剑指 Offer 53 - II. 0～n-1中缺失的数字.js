5789c9e109c171301b26019c994440051126cc5c
JX-Zhuang <who.zhuang@gmail.com>
2020-12-02 22:45:51 +0800

    missingNumber

--- 剑指 Offer/剑指 Offer 53 - II. 0～n-1中缺失的数字.js
+++ 剑指 Offer/剑指 Offer 53 - II. 0～n-1中缺失的数字.js
@@ -3,8 +3,11 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] !== i) return i;
+    var i = 0; j = nums.length - 1;
+    while (i <= j) {
+        var mid = Math.floor((i + j) / 2);
+        if (mid === nums[mid]) i = mid + 1;
+        else j = mid - 1;
     }
-    return nums.length;
+    return i;
 };
