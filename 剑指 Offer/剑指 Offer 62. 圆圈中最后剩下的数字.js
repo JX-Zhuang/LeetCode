25cf1f939510410ef2a197df45ce8dcbbd7585a8
JX-Zhuang <who.zhuang@gmail.com>
2020-12-05 22:49:37 +0800

    lastRemaining

---
+++ 剑指 Offer/剑指 Offer 62. 圆圈中最后剩下的数字.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var lastRemaining = function (n, m) {
+    var arr = new Array(n).fill(0).map((item, i) => i);
+    var i = 0;
+    while (n > 1) {
+        i = (i + m - 1) % n;
+        arr.splice(i, 1);
+        n--;
+    }
+    return arr[0];
+};
