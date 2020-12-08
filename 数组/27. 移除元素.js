c52f955daffc255bc948641fa42f13deaf095903
JX-Zhuang <who.zhuang@gmail.com>
2020-12-08 21:28:48 +0800

    removeElement

---
+++ 数组/27. 移除元素.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var j = 0;
+    for(var i = 0;i<nums.length;i++){
+        if(nums[i]!=val){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+};
