a91261c93300d3edef71a4d9080471f5b2751728
JX-Zhuang <who.zhuang@gmail.com>
2020-12-31 16:42:34 +0800

    subarraysWithKDistinct

--- Sliding Window/992. K 个不同整数的子数组.js
+++ Sliding Window/992. K 个不同整数的子数组.js
@@ -3,31 +3,26 @@
  * @param {number} K
  * @return {number}
  */
-var atMostK = function (A, K) {
-	var left = 0, right = 0;
+var atMostK = function(A, K) {
+	var left = 0,
+		right = 0;
 	var window = {};
-	var res = 0;
-	var count = 0;
-	for (var item of A) {
-		window[item] = 0;
-	}
+	for (var item of A) window[item] = 0;
+	var count = 0,
+		res = 0;
 	while (right < A.length) {
 		var item = A[right++];
-		window[item]++;
-		if (window[item] === 1) count++;
+        if (window[item]++ === 0) count++;
 		while (count > K) {
-			var n = A[left++];
-			window[n]--;
-			if (window[n] === 0) {
-				count--;
-			}
+            var c = A[left++];
+            if (--window[c] === 0) count--;
 		}
 		res += right - left;
 	}
 	return res;
-}
-var subarraysWithKDistinct = function (A, K) {
+};
+var subarraysWithKDistinct = function(A, K) {
 	return atMostK(A, K) - atMostK(A, K - 1);
 };
-console.log(subarraysWithKDistinct([1, 2, 1, 2, 3], 2));
-subarraysWithKDistinct([1, 2, 1, 3, 4], 3);
+console.log(subarraysWithKDistinct([ 1, 2, 1, 2, 3 ], 2));
+// subarraysWithKDistinct([ 1, 2, 1, 3, 4 ], 3);
