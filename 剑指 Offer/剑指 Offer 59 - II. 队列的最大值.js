bfa0119d43823b600641a9e01879ca2ccd147581
JX-Zhuang <who.zhuang@gmail.com>
2021-01-06 15:58:41 +0800

    MaxQueue

--- 剑指 Offer/剑指 Offer 59 - II. 队列的最大值.js
+++ 剑指 Offer/剑指 Offer 59 - II. 队列的最大值.js
@@ -1,20 +1,36 @@
-var MaxQueue = function() {};
+var MaxQueue = function() {
+	this.queue = [];
+	this.maxValue = Number.MIN_SAFE_INTEGER;
+};
 
 /**
  * @return {number}
  */
-MaxQueue.prototype.max_value = function() {};
+MaxQueue.prototype.max_value = function() {
+	if (this.queue.length === 0) return -1;
+	return this.maxValue;
+};
 
 /** 
  * @param {number} value
  * @return {void}
  */
-MaxQueue.prototype.push_back = function(value) {};
+MaxQueue.prototype.push_back = function(value) {
+	this.maxValue = Math.max(value, this.maxValue);
+	this.queue.push(value);
+};
 
 /**
  * @return {number}
  */
-MaxQueue.prototype.pop_front = function() {};
+MaxQueue.prototype.pop_front = function() {
+	if (this.queue.length === 0) return -1;
+	var value = this.queue.shift();
+	if (value >= this.maxValue) {
+		this.maxValue = Math.max(...this.queue);
+	}
+    return value;
+};
 
 /**
  * Your MaxQueue object will be instantiated and called as such:
