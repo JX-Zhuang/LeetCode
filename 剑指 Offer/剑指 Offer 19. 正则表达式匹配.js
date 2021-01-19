83191a11abd2b4e32c7d81fafe99ecb4e3db68d7
JX-Zhuang <who.zhuang@gmail.com>
2021-01-20 01:19:05 +0800

    isMatch

--- 剑指 Offer/剑指 Offer 19. 正则表达式匹配.js
+++ 剑指 Offer/剑指 Offer 19. 正则表达式匹配.js
@@ -16,7 +16,33 @@
  *  *  ：
  *      1. dp[i][j] = dp[i][j-2]
  *      2. dp[i][j] = dp[i-1][j];
+ * s空，p空 ： dp[0][0] = true
+ * s空，p非空：
+ * s非空，p空：dp[x][0] = false,x!=0
+ * s非空，p非空：
  */
-var isMatch = function(s, p) {
-
+var isMatch = function (s, p) {
+    var n = s.length, m = p.length;
+    var dp = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(false));
+    for (var i = 0; i <= n; i++) {
+        for (var j = 0; j <= m; j++) {
+            if (j === 0) {
+                dp[i][j] = i === 0;
+            } else {
+                if (p[j - 1] !== '*') {
+                    if (i > 0 && (s[i - 1] === p[j - 1] || p[j - 1] === '.')) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                } else {
+                    if (j >= 2) {
+                        dp[i][j] |= dp[i][j - 2];
+                    }
+                    if (i >= 1 && j >= 2 && (s[i - 1] === p[j - 2] || p[j - 2] === '.')) {
+                        dp[i][j] = f[i - 1][j];
+                    }
+                }
+            }
+        }
+    }
+    return dp[n][m];
 };
