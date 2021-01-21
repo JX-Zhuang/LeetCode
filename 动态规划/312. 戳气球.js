365d01e4bf45cc32a9c97ca8d8418580f9de4c59
JX-Zhuang <who.zhuang@gmail.com>
2021-01-22 01:52:14 +0800

    maxCoins

--- 动态规划/312. 戳气球.js
+++ 动态规划/312. 戳气球.js
@@ -1,15 +1,19 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * dp[i][j] = dp[i][k]+dp[k][j]+nums[i]*nums[k]*nums[j]
  */
 var maxCoins = function (nums) {
-
+    nums.push(1);
+    nums.unshift(1);
+    var n = nums.length;
+    var dp = Array.from(new Array(n), () => new Array(n).fill(0));
+    for (var i = n - 3; i>=0; i--) {
+        for (var j = i + 2; j < n; j++) {
+            for (var k = i + 1; k < j; k++) {
+                dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j]);
+            }
+        }
+    }
+    return dp[0][n - 1];
 };
-[3, 1, 5, 8]
-/**
- *  [3]:3
- *  [3,1]:3*1*1+1*3*1=6
- *  [3,1,5]:3*1*5+1*3*5+1*5*1=35
- *  [3,1,5,8]:3*1*5+3*5*8+1*3*8+1*8*1=167
- *            3*1*5+1*3*5+1*5*8+1*8*1
- */
