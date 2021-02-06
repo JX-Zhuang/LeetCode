3ad0ea72d3856484bb7020a6904db97b5e058cd4
JX-Zhuang <who.zhuang@gmail.com>
2021-02-07 00:08:06 +0800

    wordBreak

--- 动态规划/140. 单词拆分 II.js
+++ 动态规划/140. 单词拆分 II.js
@@ -4,16 +4,24 @@
  * @return {string[]}
  */
 var wordBreak = function(s, wordDict) {
-    var wordDictSet = new Set(wordDict);
-    var n = s.length;
-    var dp = Array.from(new Array(n + 1), () => []);
-    dp[0] = ['']
-    for(var i = 1;i<=n;i++){
-        for(var j = 0;j<i;j++){
-            if(dp[j].length && wordDictSet.has(s.slice(j,i))){
-                dp[i] = dp[i].concat(dp[j].map(item=>item+(item?' ':'')+s.slice(j,i)));
-            }
-        }
-    }
-    return dp[n];
+	var map = new Map();
+	var wordDictSet = new Set(wordDict);
+	var n = s.length;
+	var helper = function(start) {
+		if (map.has(start)) return map.get(start);
+		if (start > n - 1) return [ [] ];
+		const res = [];
+		for (var i = start + 1; i <= n; i++) {
+			var str = s.slice(start, i);
+			if (wordDictSet.has(str)) {
+				const ans = helper(i);
+				for (var item of ans) {
+					res.push([ str ].concat(item));
+				}
+			}
+		}
+		map.set(start, res);
+		return res;
+	};
+	return helper(0).map((item) => item.join(' '));
 };
