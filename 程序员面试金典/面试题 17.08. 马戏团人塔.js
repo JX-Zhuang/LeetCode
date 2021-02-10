e81c67888b9ae728734bddea88e29a239a50fb74
JX-Zhuang <who.zhuang@gmail.com>
2021-02-10 12:26:51 +0800

    bestSeqAtIndex

---
+++ 程序员面试金典/面试题 17.08. 马戏团人塔.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @param {number[]} weight
+ * @return {number}
+ */
+// dp[i] = 1 + Math.max(...dp[j],dp[k],dp[l],...0);
+// 0<=j,k,l<=i-1 && persons[j,k,l][0,1] < persons[i][0,1]
+var bestSeqAtIndex = function(height, weight) {
+	var persons = height.map((h, i) => [ h, weight[i] ]);
+	persons.sort((a, b) => a[0] - b[0]);
+	var dp = new Array(persons.length).fill(0);
+	dp[0] = 1;
+	var ans = dp[0];
+	for (var i = 1; i < persons.length; i++) {
+		var count = 0;
+		for (var j = 0; j < i; j++) {
+			if (persons[i][0] > persons[j][0] && persons[i][1] > persons[j][1]) {
+				count = Math.max(count, dp[j]);
+			}
+		}
+		dp[i] = count + 1;
+		ans = Math.max(ans, dp[i]);
+	}
+	return ans;
+};
