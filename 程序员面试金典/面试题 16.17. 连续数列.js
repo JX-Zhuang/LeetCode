bb3bad87a43a793b94174e852d7c9223b57a348c
JX-Zhuang <who.zhuang@gmail.com>
2021-02-12 22:02:10 +0800

    maxSubArray

--- 程序员面试金典/面试题 16.17. 连续数列.js
+++ 程序员面试金典/面试题 16.17. 连续数列.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-
+	var l = nums.length;
+	var dp = new Array(l).fill(0);
+	dp[0] = nums[0];
+	var ans = dp[0];
+	for (var i = 1; i < l; i++) {
+		var t = dp[i - 1] + nums[i];
+		dp[i] = t > nums[i] ? t : nums[i];
+		ans = Math.max(dp[i], ans);
+	}
+	return ans;
 };
