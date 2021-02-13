25a3fb6949bfe09126e7e91f7ce02fe6ed94dece
JX-Zhuang <who.zhuang@gmail.com>
2021-02-13 22:24:56 +0800

    message

--- 程序员面试金典/面试题 17.16. 按摩师.js
+++ 程序员面试金典/面试题 17.16. 按摩师.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var massage = function(nums) {
-
+	var n = nums.length;
+	if (n === 0) return 0;
+	var dp = new Array(n).fill(0);
+	dp[0] = nums[0];
+	ans = dp[0];
+	for (var i = 1; i < n; i++) {
+		if (i === 1) {
+			dp[i] = Math.max(dp[0], nums[1]);
+		} else {
+			dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+		}
+		ans = Math.max(ans, dp[i]);
+	}
+	return ans;
 };
