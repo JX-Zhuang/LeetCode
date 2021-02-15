e4ede6e02ecf38398e845a963cf770025103fe8c
JX-Zhuang <who.zhuang@gmail.com>
2021-02-16 00:14:46 +0800

    LFUCache

--- 设计/460. LFU 缓存.js
+++ 设计/460. LFU 缓存.js
@@ -1,20 +1,58 @@
 /**
  * @param {number} capacity
  */
-var LFUCache = function(capacity) {};
+var LFUCache = function(capacity) {
+	this.cnt = {};
+	this.cache = [];
+	this.map = new Map();
+	this.size = capacity;
+};
 
 /** 
  * @param {number} key
  * @return {number}
  */
-LFUCache.prototype.get = function(key) {};
+LFUCache.prototype.get = function(key) {
+	var item = this.map.get(key);
+	if (item === undefined) return -1;
+	this.cnt[key]++;
+	var index = this.cache.indexOf(key);
+	if (index !== -1) {
+		this.cache.splice(index, 1);
+	}
+	this.cache.unshift(key);
+	return item;
+};
 
 /** 
  * @param {number} key 
  * @param {number} value
  * @return {void}
  */
-LFUCache.prototype.put = function(key, value) {};
+LFUCache.prototype.put = function(key, value) {
+	if (this.size === 0) return;
+	if (!this.map.has(key)) {
+		if (this.map.size === this.size) {
+			var minCountKey = this.cache[0];
+			var index = 0;
+			for (var i = 1; i < this.cache.length; i++) {
+				if (this.cnt[this.cache[i]] <= this.cnt[minCountKey]) {
+					minCountKey = this.cache[i];
+					index = i;
+				}
+			}
+			this.cache.splice(index, 1);
+			delete this.cnt[minCountKey];
+			this.map.delete(minCountKey);
+		}
+	}
+	this.map.set(key, value);
+	if (!this.cnt.hasOwnProperty(key)) {
+		this.cnt[key] = 0;
+	}
+	this.cnt[key]++;
+	this.cache.unshift(key);
+};
 
 /**
  * Your LFUCache object will be instantiated and called as such:
