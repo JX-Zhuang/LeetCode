030f2c53bf0fdce36f862de771c6d15f28cdaa5d
JX-Zhuang <who.zhuang@gmail.com>
2021-02-18 02:27:39 +0800

    LFUCache

--- 设计/460. LFU 缓存.js
+++ 设计/460. LFU 缓存.js
@@ -1,13 +1,47 @@
+class Node {
+	constructor(key, value, freq) {
+		this.key = key;
+		this.value = value;
+		this.freq = freq;
+		this.prev = null;
+		this.next = null;
+	}
+}
+class DoubleList {
+	constructor() {
+		this.headNode = new Node();
+		this.tailNode = new Node();
+		this.headNode.next = this.tailNode;
+		this.tailNode.prev = this.headNode;
+	}
+	isEmpty() {
+		return this.headNode.next.next === null;
+	}
+	// 元素添加到头部
+	addFirst(node) {
+		node.next = this.headNode.next;
+		node.prev = this.headNode;
+		this.headNode.next.prev = node;
+		this.headNode.next = node;
+	}
+	// 删除某一元素
+	remove(node) {
+		node.next.prev = node.prev;
+		node.prev.next = node.next;
+	}
+	// 返回尾部元素
+	peekLast() {
+		return this.tailNode.prev;
+	}
+}
+
 /**
  * @param {number} capacity
  */
-class DoubleList{
-	
-}
 var LFUCache = function(capacity) {
-	this.cnt = {};
-	this.cache = [];
-	this.map = new Map();
+	this.freqTable = new Map(); // freq->list<Node>  list最左边的元素是最近的
+	this.keyTable = new Map(); // key->node
+	this.minFreq = 0;
 	this.size = capacity;
 };
 
@@ -16,15 +50,20 @@ var LFUCache = function(capacity) {
  * @return {number}
  */
 LFUCache.prototype.get = function(key) {
-	var item = this.map.get(key);
-	if (item === undefined) return -1;
-	this.cnt[key]++;
-	var index = this.cache.indexOf(key);
-	if (index !== -1) {
-		this.cache.splice(index, 1);
+	if (this.size <= 0 || !this.keyTable.has(key)) return -1;
+	const node = this.keyTable.get(key);
+	const { freq, value } = node;
+	this.freqTable.get(freq).remove(node);
+	if (this.freqTable.get(freq).isEmpty()) {
+		if (this.minFreq === freq) this.minFreq++;
 	}
-	this.cache.unshift(key);
-	return item;
+	const newNode = new Node(key, value, freq + 1);
+	if (!this.freqTable.has(newNode.freq)) {
+		this.freqTable.set(newNode.freq, new DoubleList());
+	}
+	this.freqTable.get(newNode.freq).addFirst(newNode);
+	this.keyTable.set(key, newNode);
+	return value;
 };
 
 /** 
@@ -33,28 +72,34 @@ LFUCache.prototype.get = function(key) {
  * @return {void}
  */
 LFUCache.prototype.put = function(key, value) {
-	if (this.size === 0) return;
-	if (!this.map.has(key)) {
-		if (this.map.size === this.size) {
-			var minCountKey = this.cache[0];
-			var index = 0;
-			for (var i = 1; i < this.cache.length; i++) {
-				if (this.cnt[this.cache[i]] <= this.cnt[minCountKey]) {
-					minCountKey = this.cache[i];
-					index = i;
-				}
-			}
-			this.cache.splice(index, 1);
-			delete this.cnt[minCountKey];
-			this.map.delete(minCountKey);
+	if (this.size <= 0) return;
+	if (this.keyTable.has(key)) {
+		const node = this.keyTable.get(key);
+		const { freq } = node;
+		this.freqTable.get(freq).remove(node);
+		if (this.freqTable.get(freq).isEmpty()) {
+			if (this.minFreq === freq) this.minFreq++;
 		}
+		const newNode = new Node(key, value, freq + 1);
+		if (!this.freqTable.has(newNode.freq)) {
+			this.freqTable.set(newNode.freq, new DoubleList());
+		}
+		this.freqTable.get(newNode.freq).addFirst(newNode);
+		this.keyTable.set(key, newNode);
+	} else {
+		const node = new Node(key, value, 1);
+		if (this.size === this.keyTable.size) {
+			const freqList = this.freqTable.get(this.minFreq);
+			this.keyTable.delete(freqList.peekLast().key);
+			freqList.remove(freqList.peekLast());
+		}
+		if (!this.freqTable.has(node.freq)) {
+			this.freqTable.set(node.freq, new DoubleList());
+		}
+		this.freqTable.get(node.freq).addFirst(node);
+		this.minFreq = 1;
+		this.keyTable.set(key, node);
 	}
-	this.map.set(key, value);
-	if (!this.cnt.hasOwnProperty(key)) {
-		this.cnt[key] = 0;
-	}
-	this.cnt[key]++;
-	this.cache.unshift(key);
 };
 
 /**
