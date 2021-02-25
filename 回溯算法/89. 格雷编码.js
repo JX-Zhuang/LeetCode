ad5a66d237685df024b05367561a9ca390312e9c
JX-Zhuang <who.zhuang@gmail.com>
2021-02-25 23:52:41 +0800

    grayCode

--- 回溯算法/89. 格雷编码.js
+++ 回溯算法/89. 格雷编码.js
@@ -2,23 +2,14 @@
  * @param {number} n
  * @return {number[]}
  */
-var grayCode = function(n) {
-	var l = Math.pow(2, n);
-	var arr = Array.from(new Array(l), (i, index) => index);
-	var ans;
-	var backtrack = function(track) {
-		if (track.length === l) {
-			ans = track;
-			return;
+var grayCode = function (n) {
+	var head = 1;
+	var ans = [0];
+	for (var i = 0; i < n; i++) {
+		for (var j = ans.length - 1; j >= 0; j--) {
+			ans.push(head + ans[j]);
 		}
-		for (var i = 0; i < l; i++) {
-			var num = arr[i];
-			if (track.includes(num)) continue;
-            console.log(i,track[track.length - 1])
-			if (Math.abs(num - track[track.length - 1]) > 1) continue;
-			backtrack(track.concat(num));
-		}
-	};
-	backtrack([ 0 ]);
+		head <<= 1;
+	}
 	return ans;
 };
