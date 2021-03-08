e03e7ed694d9dc4ffce09b08d2aeb074342c335b
JX-Zhuang <who.zhuang@gmail.com>
2021-03-08 22:50:30 +0800

    maxLength

--- 回溯算法/1239. 串联字符串的最大长度.js
+++ 回溯算法/1239. 串联字符串的最大长度.js
@@ -2,6 +2,19 @@
  * @param {string[]} arr
  * @return {number}
  */
-var maxLength = function(arr) {
-
+var maxLength = function (arr) {
+    var ans = 0;
+    var collect = [''];
+    for (var s of arr) {
+        var add = [];
+        for (var c of collect) {
+            var t = s + c;
+            if (t.length === new Set(t).size) {
+                add.push(t);
+                ans = Math.max(t.length, ans);
+            }
+        }
+        collect.push(...add);
+    }
+    return ans;
 };
