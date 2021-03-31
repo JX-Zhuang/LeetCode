fa0f73fd8fc4509a3da4698392d9b63ca9ecb6c4
JX-Zhuang <who.zhuang@gmail.com>
2021-03-31 21:31:40 +0800

    isUgly

--- 哈希表/205. 同构字符串.js
+++ 哈希表/205. 同构字符串.js
@@ -3,14 +3,14 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    var m1 = {},m2 = {};
-    for(var i = 0;i<s.length;i++){
-        if(m1.hasOwnProperty(s[i])){
-           if(m1[s[i]]!==t[i]) return false; 
+var isIsomorphic = function (s, t) {
+    var m1 = {}, m2 = {};
+    for (var i = 0; i < s.length; i++) {
+        if (m1.hasOwnProperty(s[i])) {
+            if (m1[s[i]] !== t[i]) return false;
         }
-        if(m2.hasOwnProperty(t[i])){
-            if(m2[t[i]]!==s[i]) return false;
+        if (m2.hasOwnProperty(t[i])) {
+            if (m2[t[i]] !== s[i]) return false;
         }
         m1[s[i]] = t[i];
         m2[t[i]] = s[i];
