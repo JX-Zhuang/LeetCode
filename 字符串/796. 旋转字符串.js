43ebfcb06da1707e3acb910390f16fd4661d3581
JX-Zhuang <who.zhuang@gmail.com>
2021-04-01 20:34:09 +0800

    rotateString

--- 字符串/796. 旋转字符串.js
+++ 字符串/796. 旋转字符串.js
@@ -4,5 +4,12 @@
  * @return {boolean}
  */
 var rotateString = function (A, B) {
-
+    if (A.length !== B.length) return false;
+    if (A === B) return true;
+    var oldA = A;
+    while (true) {
+        A = A.slice(1) + A[0];
+        if (A === B) return true;
+        if (oldA === A) return false;
+    }
 };
