977eeeaa56c9fc654f3403fcf6c8146885f63f64
JX-Zhuang <who.zhuang@gmail.com>
2021-04-03 23:12:30 +0800

    checkPerfectNumber

--- 数学/507. 完美数.js
+++ 数学/507. 完美数.js
@@ -2,6 +2,16 @@
  * @param {number} num
  * @return {boolean}
  */
-var checkPerfectNumber = function(num) {
-
+var checkPerfectNumber = function (num) {
+    if (num <= 1) return false;
+    var ans = 0;
+    for (var i = 1; i * i <= num; i++) {
+        if (num % i == 0) {
+            ans += i;
+            if (i * i != num) {
+                ans += num / i
+            }
+        }
+    }
+    return ans - num === num;
 };
