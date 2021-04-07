bdadf8f1de67ce354d75fe31d0127c27b7f57e11
JX-Zhuang <who.zhuang@gmail.com>
2021-04-07 22:59:38 +0800

    isUnique

--- 程序员面试金典/面试题 01.01. 判定字符是否唯一.js
+++ 程序员面试金典/面试题 01.01. 判定字符是否唯一.js
@@ -2,6 +2,12 @@
  * @param {string} astr
  * @return {boolean}
  */
-var isUnique = function(astr) {
-
+var isUnique = function (astr) {
+    var mark = 0;
+    for (var item of astr) {
+        var bit = item.charCodeAt() - 'a'.charCodeAt();
+        if ((mark & 1 << bit) != 0) return false;
+        mark |= 1 << bit;
+    }
+    return true;
 };
