bbb55edb4c4d0c5c6d9ed5b684764b80499cf3ab
JX-Zhuang <who.zhuang@gmail.com>
2021-04-15 00:12:39 +0800

    longestWPI

--- 栈/1124. 表现良好的最长时间段.js
+++ 栈/1124. 表现良好的最长时间段.js
@@ -4,17 +4,21 @@
  */
 var longestWPI = function (hours) {
     var ans = 0;
-    var score = hours.map(h => h <= 8 ? 1 : -1);
-    var prevsum = score.reduce((a, b) => a + b, 0);
+    var score = hours.map(h => h > 8 ? 1 : -1);
+    var prevsum = [0];
+    for (var i = 1; i <= hours.length; i++) {
+        prevsum[i] = prevsum[i - 1] + score[i - 1];
+    }
     var stack = [];
-    for(var i = 0;i<prevsum.length;i++){
-        if(stack.length === 0 && prevsum[i]<prevsum[i-1]){
+    for (var i = 0; i < prevsum.length; i++) {
+        const last = stack[stack.length - 1];
+        if (!stack.length || prevsum[i] < prevsum[last]) {
             stack.push(i);
         }
     }
-    for(var i = 0;i<prevsum.length;i++){
-        while(stack.length){
-            
+    for (var i = prevsum.length - 1; i >= 0 && i > ans; i--) {
+        while (stack.length && prevsum[stack[stack.length - 1]] < prevsum[i]) {
+            ans = Math.max(ans, i - stack.pop());
         }
     }
     return ans;
