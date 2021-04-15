93c6a6c8c5633ff6294449994363046d7f4fb8e3
JX-Zhuang <who.zhuang@gmail.com>
2021-04-15 22:59:52 +0800

    minRemoveToMakeValid

--- 栈/1249. 移除无效的括号.js
+++ 栈/1249. 移除无效的括号.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {string}
  */
-var minRemoveToMakeValid = function(s) {
-
+var minRemoveToMakeValid = function (s) {
+    var stack = [];
+    var m = {};
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else if (s[i] === ')') {
+            if (!stack.length) continue;
+            m[stack.pop()] = true;
+            m[i] = true;
+        }
+    }
+    var ans = '';
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === ')') {
+            if (m[i]) {
+                ans += s[i];
+            }
+        } else {
+            ans += s[i];
+        }
+    }
+    return ans;
 };
