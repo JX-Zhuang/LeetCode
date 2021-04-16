df2788ddc26a82b1b6be924058ef24450c2a4219
JX-Zhuang <who.zhuang@gmail.com>
2021-04-17 00:44:23 +0800

    maxWidthRamp

--- 栈/962. 最大宽度坡.js
+++ 栈/962. 最大宽度坡.js
@@ -2,6 +2,18 @@
  * @param {number[]} A
  * @return {number}
  */
-var maxWidthRamp = function(A) {
-
+var maxWidthRamp = function (A) {
+    var stack = [];
+    for (var i = 0; i < A.length; i++) {
+        if (!stack.length || A[stack[stack.length - 1]] > A[i]) {
+            stack.push(i);
+        }
+    }
+    var ans = 0;
+    for (var i = A.length - 1; i >= 0; i--) {
+        while (stack.length && A[stack[stack.length - 1]] <= A[i]) {
+            ans = Math.max(ans, i - stack.pop());
+        }
+    }
+    return ans;
 };
