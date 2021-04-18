781dc41985681772d4d7e7d26ffa739d90b00a89
JX-Zhuang <who.zhuang@gmail.com>
2021-04-18 23:07:01 +0800

    removeDuplicates

--- 栈/1209. 删除字符串中的所有相邻重复项 II.js
+++ 栈/1209. 删除字符串中的所有相邻重复项 II.js
@@ -3,6 +3,20 @@
  * @param {number} k
  * @return {string}
  */
-var removeDuplicates = function(s, k) {
-
+var removeDuplicates = function (s, k) {
+    var stack = [];
+    var lastS = '', num = 0;
+    for (var i = 0; i < s.length; i++) {
+        stack.push(s[i]);
+        if (s[i] !== lastS) num = 0;
+        num++;
+        if (num === k) {
+            while (num !== 0) {
+                stack.pop();
+                num--;
+            }
+        }
+        lastS = stack[stack.length - 1];
+    }
+    return stack.join('');
 };
