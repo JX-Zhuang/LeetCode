4b93704c94367960492997394fc0c7fa94aeac98
JX-Zhuang <who.zhuang@gmail.com>
2021-04-20 23:59:20 +0800

    reverseParentheses

--- 栈/1190. 反转每对括号间的子串.js
+++ 栈/1190. 反转每对括号间的子串.js
@@ -16,3 +16,12 @@ var reverseParentheses = function (s) {
     }
     return stack.join('');
 };
+// "(ed(et(oc))el)"
+// 1.stack = [(,e,d,(,e,t,(,o,c];
+// 2.temp = [c,o]
+//     stack=[(,e,d,(,e,t,c,o];
+// 3.temp = [o,c,t,e]
+// stack = [(,e,d,o,c,t,e]
+// 4.stack = [(,e,d,o,c,t,e,e,l]
+// 5.temp = [l,e,e,t,c,o,d,e]
+// statck = [l,e,e,t,c,o,d,e]
