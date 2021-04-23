4f389be66bd8f16231d60d420e330f1fab5b2ee9
JX-Zhuang <who.zhuang@gmail.com>
2021-04-23 08:17:30 +0800

    trap

--- 程序员面试金典/面试题 17.21. 直方图的水量.js
+++ 程序员面试金典/面试题 17.21. 直方图的水量.js
@@ -2,6 +2,19 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-
+var trap = function (height) {
+    var stack = [];
+    var ans = 0;
+    for (var i = 0; i < height.length; i++) {
+        while (stack.length && height[i] > height[stack[stack.length - 1]]) {
+            const top = stack.pop();
+            if (!stack.length) break;
+            const left = stack[stack.length - 1];
+            const width = i - left - 1;
+            const currentHeight = Math.min(height[left], height[i]) - height[top];
+            ans += currentHeight * width;
+        }
+        stack.push(i);
+    }
+    return ans;
 };
