e9091cf2193bf5ebb9a0a7aefd8e5a6cc1eae6e5
JX-Zhuang <who.zhuang@gmail.com>
2021-04-25 22:44:27 +0800

    countOfAtoms

--- 栈/726. 原子的数量.js
+++ 栈/726. 原子的数量.js
@@ -2,6 +2,55 @@
  * @param {string} formula
  * @return {string}
  */
+var isNumber = function (c) {
+    return c.match(/[2-9]/);
+};
+var isLowerCase = function (c) {
+    return c.match(/[a-z]/);
+};
+var isUpperCase = function (c) {
+    return c.match(/[A-Z]/);
+};
+var updateObj = function (obj, key, count) {
+    if (!obj.hasOwnProperty(key)) {
+        obj[key] = 0;
+    }
+    obj[key] += Number(count);
+};
 var countOfAtoms = function (formula) {
-
+    var obj = {};
+    var i = 0;
+    var recursion = function (formula) {
+        var obj = {};
+        var n = formula.length;
+        while (i < n && formula[i] !== ')') {
+            if (formula[i] === '(') {
+                const count = recursion(formula);
+                for (var key in count) {
+                    updateObj(obj, key, count[key]);
+                }
+            } else {
+                var item = formula[i];
+                i++;
+                while (isLowerCase(formula[i])) {
+                    item += formula[i++];
+                }
+                var count = 1;
+                while (isNumber(formula[i])) {
+                    i++;
+                    count++;
+                }
+                updateObj(obj, item, count);
+            }
+        }
+        var count = 1;
+        while (isNumber(formula[i])) {
+            i++;
+            count++;
+        }
+        for (var key in obj) {
+            obj[key] *= count;
+        }
+        return obj;
+    }
 };
