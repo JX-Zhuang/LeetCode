6ddb7d74c1f4a871d2d7a3d9489aa096504f28ea
JX-Zhuang <who.zhuang@gmail.com>
2021-04-28 23:19:51 +0800

    calculate

--- 栈/772. 基本计算器 III.js
+++ 栈/772. 基本计算器 III.js
@@ -2,9 +2,14 @@
  * @param {string} s
  * @return {number}
  */
+var operators = {
+    '+': 0,
+    '-': 0,
+    '/': 1,
+    '*': 1
+}
 var priority = function (o1, o2) {
-    if (o1 === '*' || o1 === '/') return true;
-    return false;
+    return operators[o1] - operators[o2];
 }
 var cal = function (n1, n2, operator) {
     switch (operator) {
@@ -15,42 +20,54 @@ var cal = function (n1, n2, operator) {
         case '*':
             return n1 * n2;
         case '/':
-            return Math.floor(n1 / n2);
+            const result = n1 / n2;
+            return result > 0 ? Math.floor(n1 / n2) : 0;
     }
 }
 var isNumber = function (n) {
     if (n === undefined) return false;
     return n.match(/[0-9]/);
 }
+
 var calculate = function (s) {
-    var numberStack = [],
-        operatorStack = [];
-    for (var i = 0; i < s.length; i++) {
-        var item = s[i];
-        if (isNumber(item)) {
-            while (isNumber(s[i + 1])) {
-                item += s[++i];
-            }
-            numberStack.push(Number(item));
-        } else {
-            if (operatorStack.length === 0) {
+    var i = 0;
+    var recursion = function (s) {
+        var numberStack = [],
+            operatorStack = [];
+        while (i < s.length && s[i] !== ')') {
+            var item = s[i];
+            if (item === '(') {
+                i++;
+                numberStack.push(recursion(s));
+                i++;
+            } else if (isNumber(item)) {
+                while (isNumber(s[i + 1])) {
+                    item += s[++i];
+                }
+                numberStack.push(Number(item));
+                i++;
+            } else {
+                i++;
+                if (operatorStack.length === 0) {
+                    operatorStack.push(item);
+                    continue;
+                }
+                while (priority(operatorStack[operatorStack.length - 1], item) >= 0) {
+                    const n1 = numberStack.pop(),
+                        n2 = numberStack.pop();
+                    const n = cal(n2, n1, operatorStack.pop());
+                    numberStack.push(n);
+                }
                 operatorStack.push(item);
-                continue;
             }
-            while (priority(operatorStack[operatorStack.length - 1])) {
-                const n1 = numberStack.pop(),
-                    n2 = numberStack.pop();
-                const n = cal(n2, n1, operatorStack.pop());
-                numberStack.push(n);
-            }
-            operatorStack.push(item);
         }
-    }
-    while (operatorStack.length) {
-        const n1 = numberStack.pop(),
-            n2 = numberStack.pop();
-        const n = cal(n2, n1, operatorStack.pop());
-        numberStack.push(n);
-    }
-    return numberStack.pop();
-};
+        while (operatorStack.length) {
+            const n1 = numberStack.pop(),
+                n2 = numberStack.pop();
+            const n = cal(n2, n1, operatorStack.pop());
+            numberStack.push(n);
+        }
+        return numberStack.pop();
+    };
+    return recursion(s);
+}
