b0f4cd8793ce3496f00e1b4fa6a5bf90b9ab1bc2
JX-Zhuang <who.zhuang@gmail.com>
2021-04-30 23:16:41 +0800

    shortestSubarray

--- 队列/862. 和至少为 K 的最短子数组.js
+++ 队列/862. 和至少为 K 的最短子数组.js
@@ -3,6 +3,17 @@
  * @param {number} K
  * @return {number}
  */
-var shortestSubarray = function(A, K) {
-
+var shortestSubarray = function (A, K) {
+    var queue = [];
+    var result = A.length + 1;
+    var sum = 0;
+    for (var num of A) {
+        queue.push(num);
+        sum += num;
+        while (sum <= K) {
+            if (sum === K) result = Math.min(result, queue.length);
+            sum -= queue.shift();
+        }
+    }
+    return result > A.length ? -1 : result;
 };
