da669fd2230719ba6ea7fb22bf553fd94ab17931
JX-Zhuang <who.zhuang@gmail.com>
2021-05-03 18:05:41 +0800

    SnakeGame

--- 队列/353. 贪吃蛇.js
+++ 队列/353. 贪吃蛇.js
@@ -8,8 +8,11 @@
  * @param {number} height
  * @param {number[][]} food
  */
-var SnakeGame = function(width, height, food) {
-    
+var SnakeGame = function (width, height, food) {
+    this.width = width;
+    this.height = height;
+    this.food = [...food];
+    this.sanke = [[0, 0]];
 };
 
 /**
@@ -20,11 +23,44 @@ var SnakeGame = function(width, height, food) {
  * @param {string} direction
  * @return {number}
  */
-SnakeGame.prototype.move = function(direction) {
-    
+SnakeGame.prototype.move = function (direction) {
+    var head = this.sanke[0];
+    var newHead;
+    switch (direction) {
+        case 'L':
+            newHead = [head[0], head[1] - 1];
+            break;
+        case 'R':
+            newHead = [head[0], head[1] + 1];
+            break;
+        case 'U':
+            newHead = [head[0] - 1, head[1]];
+            break;
+        default:
+            newHead = [head[0] + 1, head[1]];
+    }
+    //撞墙
+    if (newHead[0] < 0 || newHead[0] >= this.height || newHead[1] < 0 || newHead[1] >= this.width) return -1;
+    if (this.food[0] && (newHead[0] === this.food[0][0] && newHead[1] === this.food[0][1])) {
+        this.food.shift();
+        this.sanke.unshift(newHead);
+        return this.sanke.length - 1;
+    }
+    //撞自己
+    if (this.sanke.length > 4) {
+        for (var i = 0; i < this.sanke.length - 1; i++) {
+            var item = this.sanke[i];
+            if (item[0] === newHead[0] && item[1] === newHead[1]) {
+                return -1;
+            }
+        }
+    }
+    this.sanke.pop();
+    this.sanke.unshift(newHead);
+    return this.sanke.length - 1;
 };
 
-/** 
+/**
  * Your SnakeGame object will be instantiated and called as such:
  * var obj = new SnakeGame(width, height, food)
  * var param_1 = obj.move(direction)
