1349159e3e1a709ea5e1b2efe56dc2df7aaa8b0b
JX-Zhuang <who.zhuang@gmail.com>
2021-05-04 22:22:48 +0800

    topKFrequent

--- 哈希表/347. 前 K 个高频元素.js
+++ 哈希表/347. 前 K 个高频元素.js
@@ -3,6 +3,27 @@
  * @param {number} k
  * @return {number[]}
  */
- var topKFrequent = function(nums, k) {
-
+var topKFrequent = function (nums, k) {
+    var map = {};
+    for (var n of nums) {
+        if (!map.hasOwnProperty(n)) {
+            map[n] = 0;
+        }
+        map[n]++;
+    }
+    var o = {};
+    for (var key in map) {
+        const count = map[key];
+        if (!o.hasOwnProperty(count)) {
+            o[count] = [];
+        }
+        o[count].push(key);
+    }
+    const counts = Object.keys(o).sort((a, b) => b - a);
+    let result = [];
+    for (const item of counts) {
+        result = result.concat(o[item]);
+        if (result.length >= k) return result;
+    }
+    return result;
 };
