d0cd12ea2e01277f80ee2ac2db006a047c268812
JX-Zhuang <who.zhuang@gmail.com>
2021-05-05 22:23:04 +0800

    numRabbits

--- 哈希表/781. 森林中的兔子.js
+++ 哈希表/781. 森林中的兔子.js
@@ -2,6 +2,15 @@
  * @param {number[]} answers
  * @return {number}
  */
- var numRabbits = function(answers) {
-
+ var numRabbits = function (answers) {
+    var nums = {};
+    for (var i of answers) {
+        var item = i + 1;
+        if (!nums.hasOwnProperty(item)) nums[item] = 0;
+        nums[item]++;
+    }
+    return Object.keys(nums).reduce((prev, current) => {
+        var num = Math.ceil(nums[current] / current) * current;
+        return num + prev;
+    }, 0);
 };
