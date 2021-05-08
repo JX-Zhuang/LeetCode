87ae216bd1c7cd30c98f4cce1d1de7ed0615824c
JX-Zhuang <who.zhuang@gmail.com>
2021-05-08 23:57:36 +0800

    fourSum

--- 哈希表/18. 四数之和.js
+++ 哈希表/18. 四数之和.js
@@ -5,29 +5,26 @@
  */
 var fourSum = function (nums, target) {
     nums.sort((a, b) => a - b);
-    var o = {};
-    for (var n of nums) {
-        o[target - n] = n;
-    }
     var result = [];
-    var used = {};
     for (var i = 0; i < nums.length - 3; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) continue;
         for (var j = i + 1; j < nums.length - 2; j++) {
             if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-            var sum1 = nums[i] + nums[j];
-            var m = {};
-            var t = target - sum1;
-            for (var h = j + 1; h < nums.length; h++) {
-                console.log(h > j + 1)
-                if (h > j + 1 && nums[h] === nums[h - 1]) continue;
-                var item = nums[h];
-                var key = t - item;
-                if (key in m) {
-                    var arr = [nums[i], nums[j], key, item];
-                    result.push(arr);
+            var left = j + 1, right = nums.length - 1;
+            while (left < right) {
+                var sum = nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+                    while (left < right && nums[left] === nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] === nums[right - 1]) {
+                        right--;
+                    }
+                } else if (sum > target) {
+                    right--;
                 } else {
-                    m[item] = h;
+                    left++;
                 }
             }
         }
