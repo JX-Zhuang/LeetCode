0c27f246f9f46f73a3d7e3f63c97dc31504e94c7
JX-Zhuang <who.zhuang@gmail.com>
2021-05-08 22:19:55 +0800

    fourSum

--- 哈希表/18. 四数之和.js
+++ 哈希表/18. 四数之和.js
@@ -10,19 +10,22 @@ var fourSum = function (nums, target) {
         o[target - n] = n;
     }
     var result = [];
+    var used = {};
     for (var i = 0; i < nums.length - 3; i++) {
-        if (nums[i] > target) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
         for (var j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
             var sum1 = nums[i] + nums[j];
-            if (sum1 > target) break;
             var m = {};
             var t = target - sum1;
             for (var h = j + 1; h < nums.length; h++) {
+                console.log(h > j + 1)
+                if (h > j + 1 && nums[h] === nums[h - 1]) continue;
                 var item = nums[h];
-                if (item + sum1 > target) break;
                 var key = t - item;
                 if (key in m) {
-                    result.push([nums[i], nums[j], key, item]);
+                    var arr = [nums[i], nums[j], key, item];
+                    result.push(arr);
                 } else {
                     m[item] = h;
                 }
