3659e3169c187717a394037dbcd53782cb35f475
JX-Zhuang <who.zhuang@gmail.com>
2021-05-17 19:47:57 +0800

    longestPalindrome

--- 哈希表/409. 最长回文串.js
+++ 哈希表/409. 最长回文串.js
@@ -2,6 +2,20 @@
  * @param {string} s
  * @return {number}
  */
- var longestPalindrome = function(s) {
-
+var longestPalindrome = function (s) {
+    var m = {};
+    for (var i = 0; i < s.length; i++) {
+        if (!m.hasOwnProperty(s[i])) m[s[i]] = 0;
+        m[s[i]]++;
+    }
+    var ans = 0;
+    var l = 0;
+    for (var k in m) {
+        if (m[k] % 2) {
+            l = Math.max(l, m[k])
+        } else {
+            ans += m[k];
+        }
+    }
+    return ans + l;
 };
