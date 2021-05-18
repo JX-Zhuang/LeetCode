6a2c8d3ca96b56b796758e302e60acd727a297fd
JX-Zhuang <who.zhuang@gmail.com>
2021-05-18 23:10:40 +0800

    intersect

--- 哈希表/350. 两个数组的交集 II.js
+++ 哈希表/350. 两个数组的交集 II.js
@@ -3,6 +3,20 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function(nums1, nums2) {
-
+var intersect = function (nums1, nums2) {
+    var ans = [];
+    var m2 = {};
+    for (var n of nums2) {
+        if (!m2.hasOwnProperty(n)) {
+            m2[n] = 0;
+        }
+        m2[n]++;
+    }
+    for (var k of nums1) {
+        if (m2[k] > 0) {
+            ans.push(k);
+            m2[k]--;
+        }
+    }
+    return ans;
 };
