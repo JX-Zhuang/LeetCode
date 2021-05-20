00f7213622df0bcd3b3cf991d15ddce7fc3a3987
JX-Zhuang <who.zhuang@gmail.com>
2021-05-20 22:25:04 +0800

    findKthPositive

--- 哈希表/1539. 第 k 个缺失的正整数.js
+++ 哈希表/1539. 第 k 个缺失的正整数.js
@@ -3,6 +3,13 @@
  * @param {number} k
  * @return {number}
  */
-var findKthPositive = function(arr, k) {
-
+var findKthPositive = function (arr, k) {
+    var length = arr[arr.length - 1] + k;
+    var a = new Array(length).fill(0).map((_, index) => index + 1);
+    for (var i = 0; i < a.length; i++) {
+        if (!arr.includes(a[i])) {
+            k--;
+            if (k === 0) return a[i];
+        }
+    }
 };
