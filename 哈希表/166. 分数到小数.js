c751627063993700b775ab13419b0e2599980cb7
JX-Zhuang <who.zhuang@gmail.com>
2021-05-21 23:43:54 +0800

    fractionToDecimal

--- 哈希表/166. 分数到小数.js
+++ 哈希表/166. 分数到小数.js
@@ -4,5 +4,26 @@
  * @return {string}
  */
 var fractionToDecimal = function (numerator, denominator) {
-    var ans = String(numerator / denominator);
+    if (numerator === 0) return '0';
+    var ans = [];
+    if (numerator < 0 ^ denominator < 0) ans.push('-');
+    numerator = Math.abs(numerator);
+    denominator = Math.abs(denominator);
+    ans.push(Math.floor(numerator / denominator));
+    var remainder = numerator % denominator;
+    if (remainder === 0) return ans.join('');
+    ans.push('.');
+    var map = {};
+    while (remainder !== 0) {
+        if (map.hasOwnProperty(remainder)) {
+            ans.splice(map[remainder], 0, '(');
+            ans.push(')');
+            break;
+        }
+        map[remainder] = ans.length;
+        remainder *= 10;
+        ans.push(Math.floor(remainder / denominator));
+        remainder %= denominator;
+    }
+    return ans.join('');
 };
