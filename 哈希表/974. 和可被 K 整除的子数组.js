d7528912b67d578e83a23db364d2d32e397eaffe
JX-Zhuang <who.zhuang@gmail.com>
2021-05-22 22:43:48 +0800

    subarraysDivByK

--- 哈希表/974. 和可被 K 整除的子数组.js
+++ 哈希表/974. 和可被 K 整除的子数组.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 var subarraysDivByK = function (nums, k) {
-
+    var ans = 0;
+    for (var i = 0; i < nums.length; i++) {
+        var sum = nums[i];
+        if (sum % k === 0) {
+            ans++;
+        }
+        for (var j = i + 1; j < nums.length; j++) {
+            sum += nums[j];
+            if (sum % k === 0) {
+                ans++;
+            }
+        }
+    }
+    return ans;
 };
