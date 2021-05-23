a73698a3e89c9f10fc2ee53b33bfa395b43772e5
JX-Zhuang <who.zhuang@gmail.com>
2021-05-23 17:24:41 +0800

    wordPattern

--- 哈希表/290. 单词规律.js
+++ 哈希表/290. 单词规律.js
@@ -3,6 +3,19 @@
  * @param {string} s
  * @return {boolean}
  */
- var wordPattern = function(pattern, s) {
-
+var wordPattern = function (pattern, s) {
+    var arr = s.split(' ');
+    var m = {};
+    var n = {};
+    if (pattern.length !== arr.length) return false;
+    for (var i = 0; i < pattern.length; i++) {
+        var item = pattern[i];
+        if (!m.hasOwnProperty(item) && !n.hasOwnProperty(arr[i])) {
+            m[item] = arr[i];
+            n[arr[i]] = item;
+        } else {
+            if (m[item] !== arr[i]) return false;
+        }
+    }
+    return true;
 };
