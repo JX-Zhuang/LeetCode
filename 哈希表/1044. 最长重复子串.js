59193adacf3f1417c27a8eb81b571e6d4173d8af
JX-Zhuang <who.zhuang@gmail.com>
2021-05-23 22:22:54 +0800

    longestDupSubstring

--- 哈希表/1044. 最长重复子串.js
+++ 哈希表/1044. 最长重复子串.js
@@ -5,18 +5,20 @@
 var longestDupSubstring = function (s) {
     var m = {};
     var ans = '';
-    for (var i = 0; i < s.length - 1; i++) {
-        var str = s[i];
-        for (var j = i + 1; j < s.length; j++) {
-            str += s[j];
-            if (!m.hasOwnProperty(str)) {
-                m[str] = 0;
-            }
-            m[str]++;
-            if (m[str] > 1 && str.length > ans.length) {
-                ans = str;
-            }
+    for (var i = 0; i < s.length; i++) {
+        var item = s[i];
+        m = { ...m };
+        for (var key in m) {
+            var newK = key + item;
+            m[newK] = m[key];
         }
+        if (!m.hasOwnProperty(item)) {
+            m[item] = 0;
+        }
+        m[item]++;
+    }
+    for (var key in m) {
+        if (m[key] > 1 && key.length > ans.length) ans = key;
     }
     return ans;
 };
