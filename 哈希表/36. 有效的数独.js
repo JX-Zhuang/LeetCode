393c7fe1c6c65d26f8f6440b92a4a539591332d5
JX-Zhuang <who.zhuang@gmail.com>
2021-05-27 23:25:54 +0800

    isValidSudoku

--- 哈希表/36. 有效的数独.js
+++ 哈希表/36. 有效的数独.js
@@ -3,5 +3,32 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-
+    for(var i = 0;i<9;i++){
+        var o = {};
+        for(var j = 0;j<9;j++){
+            var item = board[i][j];
+            if(o[item]) return false;
+        }
+    }
+    for(var i = 0;i<9;i++){
+        var o = {};
+        for(var j = 0;j<9;j++){
+            var item = board[j][i];
+            if(o[item]) return false;
+        }
+    }
+    for(var i = 0;i<9;i+=3){
+        for(var j = 0;j<9;j+=3){
+            var o = {};
+            for(var h = 0;h<3;h++){
+                var item = board[i+h][j];
+                if(o[item]) return false;
+            }
+            for(var h = 0;h<3;h++){
+                var item = board[i][j+h];
+                if(o[item]) return false;
+            }
+        }
+    }
+    return true;
 };
