d60984136680686a931dcb1ebdcb06ae6123e91f
JX-Zhuang <who.zhuang@gmail.com>
2021-05-28 22:50:09 +0800

    isValidSudoku

--- 哈希表/36. 有效的数独.js
+++ 哈希表/36. 有效的数独.js
@@ -2,32 +2,24 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-    for(var i = 0;i<9;i++){
-        var o = {};
-        for(var j = 0;j<9;j++){
-            var item = board[i][j];
-            if(o[item]) return false;
-        }
+var isValidSudoku = function (board) {
+    var rows = [], cols = [], boxes = [];
+    for (var i = 0; i < board.length; i++) {
+        rows[i] = {};
+        cols[i] = {};
+        boxes[i] = {};
     }
-    for(var i = 0;i<9;i++){
-        var o = {};
-        for(var j = 0;j<9;j++){
-            var item = board[j][i];
-            if(o[item]) return false;
-        }
-    }
-    for(var i = 0;i<9;i+=3){
-        for(var j = 0;j<9;j+=3){
-            var o = {};
-            for(var h = 0;h<3;h++){
-                var item = board[i+h][j];
-                if(o[item]) return false;
-            }
-            for(var h = 0;h<3;h++){
-                var item = board[i][j+h];
-                if(o[item]) return false;
-            }
+    var l1 = board.length, l2 = board[0].length;
+    var item, boxIndex;
+    for (var i = 0; i < l1; i++) {
+        for (var j = 0; j < l2; j++) {
+            item = board[i][j];
+            if (item === '.') continue;
+            boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+            if (rows[i][item] || cols[j][item] || boxes[boxIndex][item]) return false;
+            rows[i][item] = true;
+            cols[j][item] = true;
+            boxes[boxIndex][item] = true;
         }
     }
     return true;
