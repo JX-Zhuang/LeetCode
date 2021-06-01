1410a6c0f3b618780619d419947ea813e0e162bd
JX-Zhuang <who.zhuang@gmail.com>
2021-06-01 22:44:21 +0800

    isValidSudoku

--- 哈希表/1178. 猜字谜.js
+++ 哈希表/1178. 猜字谜.js
@@ -5,13 +5,9 @@
  */
 //  单词 word 中包含谜面 puzzle 的第一个字母。
 //  单词 word 中的每一个字母都可以在谜面 puzzle 中找到。
-var isAnswer = function (word, puzzle) {
+var isAnswer = function (word, puzzle, m) {
     var first = puzzle[0];
     if (!word.includes(first)) return false;
-    var m = {};
-    for (var ch of puzzle) {
-        m[ch] = true;
-    }
     for (var ch of word) {
         if (!m[ch]) return false;
     }
@@ -20,12 +16,28 @@ var isAnswer = function (word, puzzle) {
 
 var getStr = function (puzzle) {
     var str = puzzle[0];
-    str += [...new Set(puzzle.slice(1).split(""))].sort().join('');
+    str += puzzle.slice(1).split("").sort().join('');
     return str;
 }
 var findNumOfValidWords = function (words, puzzles) {
     var ans = [];
     var m = {};
+    var mapPuzzle = {};
+    var mapWord = {};
+    for(var word of words){
+        const m = {};
+        for(var w of word){
+            m[w] = true;
+        }
+        mapWord[word] = m;
+    }
+    for (var puzzle of puzzles) {
+        const m = {};
+        for (var ch of puzzle) {
+            m[ch] = true;
+        }
+        mapPuzzle[puzzle] = m;
+    }
     for (var p of puzzles) {
         var i = 0;
         var str = getStr(p);
@@ -34,7 +46,16 @@ var findNumOfValidWords = function (words, puzzles) {
             continue;
         }
         for (var w of words) {
-            if (isAnswer(w, p)) i++;
+            const first = p[0];
+            let flag = true;
+            if (!mapWord[w][first]) continue;
+            for (var ch of w) {
+                if (!mapPuzzle[p][ch]) {
+                    flag = false;
+                    break;
+                }
+            }
+            flag && i++;
         }
         m[str] = i;
         ans.push(i);
