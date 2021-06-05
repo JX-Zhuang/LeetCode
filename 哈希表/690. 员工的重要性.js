1d463b21afba37347b1255e50dd993acd9131244
JX-Zhuang <who.zhuang@gmail.com>
2021-06-05 23:36:39 +0800

    GetImportance

--- 哈希表/690. 员工的重要性.js
+++ 哈希表/690. 员工的重要性.js
@@ -12,6 +12,24 @@
  * @param {number} id
  * @return {number}
  */
-var GetImportance = function(employees, id) {
-    
+var GetImportance = function (employees, id) {
+    var map = {};
+    for (var employee of employees) {
+        map[employee.id] = employee;
+    }
+    var visited = {};
+    var total = map[id].importance;
+    var helper = function (employee) {
+        var employees = employee.subordinates;
+        if (!employees) return;
+        for (var id of employees) {
+            const item = map[id];
+            if (visited[id]) continue;
+            total += item.importance;
+            visited[id] = true;
+            helper(item);
+        }
+    }
+    helper(map[id]);
+    return total;
 };
