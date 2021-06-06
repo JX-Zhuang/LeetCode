d37edc0af03f55dff700704f891bf88857ef6c1f
JX-Zhuang <who.zhuang@gmail.com>
2021-06-06 21:52:07 +0800

    leastBricks

--- 哈希表/554. 砖墙.js
+++ 哈希表/554. 砖墙.js
@@ -2,6 +2,18 @@
  * @param {number[][]} wall
  * @return {number}
  */
-var leastBricks = function(wall) {
-
+var leastBricks = function (wall) {
+    var map = new Map();
+    for (var w of wall) {
+        var sum = 0;
+        for (var i = 0; i < w.length - 1; i++) {
+            sum += w[i];
+            map.set(sum, (map.get(sum) || 0) + 1);
+        }
+    }
+    var max = 0;
+    for (var [key, value] of map) {
+        max = Math.max(value, max);
+    }
+    return wall.length - max;
 };
