5eba57412a21b83c21076c985a8049748db80e8f
JX-Zhuang <who.zhuang@gmail.com>
2021-06-06 22:15:01 +0800

    lengthOfLongestSubstringTwoDistinct

--- 哈希表/159. 至多包含两个不同字符的最长子串.js
+++ 哈希表/159. 至多包含两个不同字符的最长子串.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var lengthOfLongestSubstringTwoDistinct = function (s) {
-    var m = {};
+    var m = new Map();
     var res = 0;
     for (var left = 0, right = 0; right < s.length; right++) {
         var item = s[right];
-        m[item] = 1 || (m[item] + 1);
-        while (Object.keys(m).length > 2) {
+        m.set(item, (m.get(item) || 0) + 1);
+        while (m.size > 2) {
             var leftItem = s[left++];
-            m[leftItem]--;
-            if (m[leftItem] === 0) delete m[leftItem];
+            m.set(leftItem, m.get(leftItem) - 1);
+            if (m.get(leftItem) === 0) m.delete(leftItem);
         }
         res = Math.max(right - left + 1, res);
     }
