ff6817e10eede681092986601bce95af54f0ceac
JX-Zhuang <who.zhuang@gmail.com>
2021-06-07 21:41:45 +0800

    findTheDifference

--- 哈希表/389. 找不同.js
+++ 哈希表/389. 找不同.js
@@ -3,6 +3,16 @@
  * @param {string} t
  * @return {character}
  */
-var findTheDifference = function(s, t) {
-
+var findTheDifference = function (s, t) {
+    var sMap = new Map();
+    for (var item of s) {
+        sMap.set(item, (sMap.get(item) || 0) + 1);
+    }
+    var tMap = new Map();
+    for (var item of t) {
+        tMap.set(item, (tMap.get(item) || 0) + 1);
+    }
+    for (var [key, value] of tMap) {
+        if (value !== sMap.get(key)) return key;
+    }
 };
