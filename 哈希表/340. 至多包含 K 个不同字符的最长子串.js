9e8d1770c7ba629aef3f5beb55b93d576ee33067
JX-Zhuang <who.zhuang@gmail.com>
2021-06-08 22:57:02 +0800

    lengthOfLongestSubstringKDistinct

--- 哈希表/340. 至多包含 K 个不同字符的最长子串.js
+++ 哈希表/340. 至多包含 K 个不同字符的最长子串.js
@@ -3,6 +3,18 @@
  * @param {number} k
  * @return {number}
  */
-var lengthOfLongestSubstringKDistinct = function(s, k) {
-
+var lengthOfLongestSubstringKDistinct = function (s, k) {
+    var map = new Map();
+    var ans = 0;
+    for (var left = 0, right = 0; right < s.length; right++) {
+        var item = s[right];
+        map.set(item, (map.get(item) || 0) + 1);
+        while (map.size > k) {
+            var itemLeft = s[left++];
+            map.set(itemLeft, map.get(itemLeft) - 1);
+            if (map.get(itemLeft) === 0) map.delete(itemLeft);
+        }
+        ans = Math.max(ans, right - left + 1);
+    }
+    return ans;
 };
