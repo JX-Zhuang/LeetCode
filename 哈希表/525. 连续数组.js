aeeb61906b555f60c1604e3e5d51f8fc49bfc353
JX-Zhuang <who.zhuang@gmail.com>
2021-06-10 00:20:46 +0800

    findMaxLength

--- 哈希表/525. 连续数组.js
+++ 哈希表/525. 连续数组.js
@@ -2,6 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxLength = function(nums) {
-
+var findMaxLength = function (nums) {
+    var m = new Map();
+    var counter = 0;
+    m.set(counter, -1);
+    var max = 0;
+    for (var i = 0; i < nums.length; i++) {
+        var num = nums[i];
+        if (num === 1) {
+            counter++;
+        } else {
+            counter--;
+        }
+        if (m.has(counter)) {
+            var index = m.get(counter);
+            max = Math.max(max, i - index);
+        } else {
+            m.set(counter, i);
+        }
+    }
+    return max;
 };
