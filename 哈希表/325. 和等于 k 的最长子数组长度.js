ccdb700f19467207c58863ed933c4a2739d1da0a
JX-Zhuang <who.zhuang@gmail.com>
2021-06-12 22:34:28 +0800

    maxSubArrayLen

--- 哈希表/325. 和等于 k 的最长子数组长度.js
+++ 哈希表/325. 和等于 k 的最长子数组长度.js
@@ -3,6 +3,18 @@
  * @param {number} k
  * @return {number}
  */
- var maxSubArrayLen = function(nums, k) {
-
+var maxSubArrayLen = function (nums, k) {
+    var ans = 0;
+    var m = {
+    };
+    var sum = 0;
+    for (var i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        m[sum] = i;
+        if (sum === k) ans = i;
+        if (m.hasOwnProperty(-k) && nums[i] !== -k) {
+            ans = Math.max(ans, i - m[-k] + 1);
+        }
+    }
+    return ans;
 };
