55c10ec01e6f98a7ddd4ca187c4c0b73956d0533
JX-Zhuang <who.zhuang@gmail.com>
2021-06-14 01:00:06 +0800

    hIndex

--- 哈希表/274. H 指数.js
+++ 哈希表/274. H 指数.js
@@ -2,6 +2,11 @@
  * @param {number[]} citations
  * @return {number}
  */
- var hIndex = function(citations) {
-
+var hIndex = function (citations) {
+    citations.sort((a, b) => a - b);
+    var i = 0;
+    while (citations.length > i && citations[citations.length - 1 - i] > i) {
+        i++;
+    }
+    return i;
 };
