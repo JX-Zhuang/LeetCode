727121b202a9c299fb1261acf2bce9598baa9ae2
JX-Zhuang <who.zhuang@gmail.com>
2021-06-13 22:49:03 +0800

    findErrorNums

--- 哈希表/645. 错误的集合.js
+++ 哈希表/645. 错误的集合.js
@@ -2,6 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var findErrorNums = function(nums) {
-
+var findErrorNums = function (nums) {
+    var m = new Set();
+    var ans = [];
+    for (var n of nums) {
+        if (m.has(n)) {
+            ans.push(n);
+        }
+        m.add(n);
+    }
+    var arr = new Array(nums.length).fill(0).map((_, i) => i + 1);
+    for (var n of arr) {
+        if (!m.has(n)) ans.push(n);
+    }
+    return ans;
 };
