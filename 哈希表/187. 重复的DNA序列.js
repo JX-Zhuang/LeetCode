d1cc956845fd88a21b4203e9fdcd991db72dfcbd
JX-Zhuang <who.zhuang@gmail.com>
2021-06-14 23:07:42 +0800

    findRepeatedDnaSequences

--- 哈希表/187. 重复的DNA序列.js
+++ 哈希表/187. 重复的DNA序列.js
@@ -2,6 +2,16 @@
  * @param {string} s
  * @return {string[]}
  */
- var findRepeatedDnaSequences = function(s) {
-
+var findRepeatedDnaSequences = function (s) {
+    var o = {};
+    for (var i = 0; i <= s.length - 10; i++) {
+        var item = s.substr(i, 10);
+        o[item] = o[item] || 0;
+        o[item]++;
+    }
+    var arr = [];
+    for (var k in o) {
+        if (o[k] > 1) arr.push(k);
+    }
+    return arr;
 };
