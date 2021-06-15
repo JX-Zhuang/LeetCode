316ab7cf61d7161dfdd621569ef01c6589e82e79
JX-Zhuang <who.zhuang@gmail.com>
2021-06-15 23:09:30 +0800

    islandPerimeter

--- 哈希表/463. 岛屿的周长.js
+++ 哈希表/463. 岛屿的周长.js
@@ -2,6 +2,35 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var islandPerimeter = function(grid) {
-
+var getLength = function (i, j, rows, cols, grid) {
+    var ans = 4;
+    var up = i - 1,
+        down = i + 1,
+        left = j - 1,
+        right = j + 1;
+    if (up >= 0) {
+        if (grid[up][j] === 1) ans--;
+    }
+    if (down < rows) {
+        if (grid[down][j] === 1) ans--;
+    }
+    if (left >= 0) {
+        if (grid[i][left] === 1) ans--;
+    }
+    if (right < cols) {
+        if (grid[i][right] === 1) ans--;
+    }
+    return ans;
+}
+var islandPerimeter = function (grid) {
+    var ans = 0;
+    var rows = grid.length;
+    var cols = grid[0].length;
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (grid[i][j] === 0) continue;
+            ans += getLength(i, j, rows, cols, grid);
+        }
+    }
+    return ans;
 };
