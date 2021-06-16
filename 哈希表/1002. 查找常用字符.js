9671c3c1da20456e8cc2664c48e76b98c70d7d9a
JX-Zhuang <who.zhuang@gmail.com>
2021-06-16 23:28:00 +0800

    commonChars

--- 哈希表/1002. 查找常用字符.js
+++ 哈希表/1002. 查找常用字符.js
@@ -2,6 +2,25 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var commonChars = function(words) {
-
+var commonChars = function (words) {
+    var map = new Map();
+    for (var w of words) {
+        var o = {};
+        for (var ch of w) {
+            if (!o.hasOwnProperty(ch)) {
+                o[ch] = 0;
+            }
+            o[ch]++;
+            var key = `${ch}_${o[ch]}`;
+            map.set(key, (map.get(key) || 0) + 1);
+        }
+    }
+    var max = words.length;
+    var ans = [];
+    for (var [_, count] of map) {
+        if (count === max) {
+            ans.push(_.split('_')[0]);
+        }
+    }
+    return ans;
 };
