d53466fea3fb9f581e5d16318c70c0b2944c66bb
JX-Zhuang <who.zhuang@gmail.com>
2021-06-18 22:50:25 +0800

    FreqStack

--- 哈希表/895. 最大频率栈.js
+++ 哈希表/895. 最大频率栈.js
@@ -1,20 +1,39 @@
-var FreqStack = function() {
-
+var FreqStack = function () {
+    this.stack = [];
+    this.map = new Map();
 };
 
 /** 
  * @param {number} val
  * @return {void}
  */
-FreqStack.prototype.push = function(val) {
-
+FreqStack.prototype.push = function (val) {
+    this.stack.push(val);
+    this.map.set(val, (this.map.get(val) || 0) + 1);
 };
 
 /**
  * @return {number}
  */
-FreqStack.prototype.pop = function() {
-
+FreqStack.prototype.pop = function () {
+    var max = 0;
+    for (var [val, count] of this.map) {
+        max = Math.max(val, count);
+    }
+    var set = new Set();
+    for (var [val, count] of this.map) {
+        if (count === max) set.add(val);
+    }
+    console.log(max, set)
+    for (var i = this.stack.length - 1; i >= 0; i--) {
+        var val = this.stack[i];
+        if (set.has(val)) {
+            this.stack.splice(i, 1);
+            this.map.set(val, this.map.get(val)--);
+            if (this.map.get(val) === 0) this.map.delete(val);
+            return val;
+        }
+    }
 };
 
 /**
