8ce3fba80e58ea933aa1bac92c3d93acf7a3349f
JX-Zhuang <who.zhuang@gmail.com>
2021-06-22 23:10:59 +0800

    tiny url

--- 哈希表/535. TinyURL 的加密与解密.js
+++ 哈希表/535. TinyURL 的加密与解密.js
@@ -4,8 +4,15 @@
  * @param {string} longUrl
  * @return {string}
  */
-var encode = function(longUrl) {
-    
+var m = new Map();
+var str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var encode = function (longUrl) {
+    var url = '';
+    for (var i = 0; i < 6; i++) {
+        url += str[parseInt(Math.random() * str.length)];
+    }
+    m.set(url, longUrl);
+    return 'http://tinyurl.com/' + url;
 };
 
 /**
@@ -14,9 +21,10 @@ var encode = function(longUrl) {
  * @param {string} shortUrl
  * @return {string}
  */
-var decode = function(shortUrl) {
-    
+var decode = function (shortUrl) {
+    return m.get(shortUrl.replace('http://tinyurl.com/', ''));
 };
+
 /**
  * Your functions will be called as such:
  * decode(encode(url));
