3add204be2434ad14d1d34433fe0e34154815b2e
JX-Zhuang <who.zhuang@gmail.com>
2021-06-24 22:31:21 +0800

    palindromePairs

--- 哈希表/336. 回文对.js
+++ 哈希表/336. 回文对.js
@@ -2,6 +2,23 @@
  * @param {string[]} words
  * @return {number[][]}
  */
-var palindromePairs = function(words) {
-
+var isPalidrome = function (word) {
+    var left = 0, right = word.length - 1;
+    while (left < right) {
+        if (word[left] !== word[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+var palindromePairs = function (words) {
+    var ans = [];
+    for (var i = 0; i < words.length; i++) {
+        for (var j = 0; j < words.length; j++) {
+            if (i !== j && isPalidrome(words[i] + words[j])) {
+                ans.push([i, j]);
+            }
+        }
+    }
+    return ans;
 };
