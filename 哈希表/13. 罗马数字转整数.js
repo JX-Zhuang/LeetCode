3fc5e150b27b3a4b965af280701124187d99c37c
JX-Zhuang <who.zhuang@gmail.com>
2021-06-25 22:43:38 +0800

    romanToInt

--- 哈希表/13. 罗马数字转整数.js
+++ 哈希表/13. 罗马数字转整数.js
@@ -2,6 +2,23 @@
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
-
+var romanToInt = function (s) {
+    var m = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    var ans = 0;
+    for (var i = 0; i < s.length; i++) {
+        if (i < s.length - 1 && m[s[i]] < m[s[i + 1]]) {
+            ans -= m[s[i]];
+        } else {
+            ans += m[s[i]];
+        }
+    };
+    return ans;
 };
