b8fe630de9bcdf3cfe31c3cf7b6ae38d13d4ab87
JX-Zhuang <who.zhuang@gmail.com>
2021-06-27 00:10:49 +0800

    numBusesToDestination

--- 哈希表/815. 公交路线.js
+++ 哈希表/815. 公交路线.js
@@ -4,6 +4,34 @@
  * @param {number} target
  * @return {number}
  */
- var numBusesToDestination = function(routes, source, target) {
-
+var numBusesToDestination = function (routes, source, target) {
+    if (source === target) return 0;
+    var map = new Map(); // 站->index[]
+    for (var i = 0; i < routes.length; i++) {
+        var item = routes[i];
+        for (var j = 0; j < item.length; j++) {
+            if (!map.has(item[j])) {
+                map.set(item[j], []);
+            }
+            map.get(item[j]).push(i);
+        }
+    }
+    var arr = map.get(source);
+    var ans = 1;
+    var visited = new Set();
+    while (arr.length) {
+        for (var i = arr.length - 1; i >= 0; i--) {
+            var index = arr.shift();
+            for (var j = 0; j < routes[index].length; j++) {
+                var item = routes[index][j];
+                if (item === target) return ans;
+                for (var key of map.get(item)) {
+                    if (!visited.has(key)) arr.push(key);
+                    visited.add(key);
+                }
+            }
+        }
+        ans++;
+    }
+    return -1;
 };
