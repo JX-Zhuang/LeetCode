5f8e94cd69e981dab0aaa21eede4b642fc3ce0e2
JX-Zhuang <who.zhuang@gmail.com>
2021-06-27 23:41:58 +0800

    firstMissingPositive

--- 哈希表/41. 缺失的第一个正数.js
+++ 哈希表/41. 缺失的第一个正数.js
@@ -2,6 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
- var firstMissingPositive = function(nums) {
-
+var firstMissingPositive = function (nums) {
+    var set = new Set();
+    var max = 1;
+    for (var n of nums) {
+        set.add(n);
+        max = Math.max(max, n);
+    }
+    for (var i = 1; i <= max; i++) {
+        if (!set.has(i)) return i;
+    }
+    return max + 1;
 };
