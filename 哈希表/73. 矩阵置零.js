5c63d14d59dfd040e0d19857a3ff1995cffc9c8c
JX-Zhuang <who.zhuang@gmail.com>
2021-07-02 23:46:09 +0800

    update

--- 哈希表/73. 矩阵置零.js
+++ 哈希表/73. 矩阵置零.js
@@ -2,6 +2,25 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
-
+var setZeroes = function (matrix) {
+    var n = matrix.length, m = matrix[0].length;
+    var zero = {
+        row: {},
+        col: {}
+    };
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < m; j++) {
+            if (matrix[i][j] === 0) {
+                zero.row[i] = true;
+                zero.col[j] = true;
+            }
+        }
+    }
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < m; j++) {
+            if (zero.row[i] || zero.col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 };
