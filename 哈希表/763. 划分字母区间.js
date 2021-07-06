e896f8c04336d9f3a6aa50b0b83ee12dea8cc751
JX-Zhuang <who.zhuang@gmail.com>
2021-07-07 00:03:19 +0800

    partitionLabels

--- 哈希表/763. 划分字母区间.js
+++ 哈希表/763. 划分字母区间.js
@@ -2,6 +2,19 @@
  * @param {string} s
  * @return {number[]}
  */
-var partitionLabels = function(s) {
-
+var partitionLabels = function (s) {
+    var ans = [];
+    var m = {};
+    for (var i = 0; i < s.length; i++) {
+        m[s[i]] = i;
+    }
+    var end = 0, start = 0;
+    for (var i = 0; i < s.length; i++) {
+        end = Math.max(end, m[s[i]]);
+        if (end === i) {
+            ans.push(end - start + 1);
+            start = end + 1;
+        }
+    }
+    return ans;
 };
