ac8f70fee7f9e5750bbcb815cccd009093bdaeb7
JX-Zhuang <who.zhuang@gmail.com>
2021-07-08 23:03:17 +0800

    deleteAndEarn

--- 哈希表/740. 删除并获得点数.js
+++ 哈希表/740. 删除并获得点数.js
@@ -2,6 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var deleteAndEarn = function(nums) {
-
+var deleteAndEarn = function (nums) {
+    var max = Math.max(...nums);
+    var arr = new Array(max + 1).fill(0);
+    for (var item of nums) {
+        arr[item] += item;
+    }
+    var current = 0, prev = 0;
+    for (var i = 0; i < arr.length; i++) {
+        var temp = current;
+        current = Math.max(current, prev + arr[i]);
+        prev = temp;
+    }
+    return current;
 };
