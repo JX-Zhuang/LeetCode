0c03f7daa83fbaec9db38ffb6f1c69dfa4e4b1aa
JX-Zhuang <who.zhuang@gmail.com>
2021-07-10 00:00:48 +0800

    checkSubarraySum

--- 哈希表/523. 连续的子数组和.js
+++ 哈希表/523. 连续的子数组和.js
@@ -4,19 +4,17 @@
  * @return {boolean}
  */
 var checkSubarraySum = function (nums, k) {
-    for (var i = 1; i < nums.length; i++) {
-        if (nums[i] === 0 && nums[i - 1] === 0) return true;
-    }
     if (nums.length === 1) return false;
-    var set = new Set();
-    var sum = nums[0];
-    set.add(sum % k);
-    for (var i = 1; i < nums.length; i++) {
-        sum += nums[i];
-        var remainder = sum % k;
-        if (remainder === 0) return true;
-        if (sum >= k && nums[i] % k && set.has(remainder)) return true;
-        set.add(remainder);
+    var map = new Map();
+    var remainder = 0;
+    map.set(0, -1);
+    for (var i = 0; i < nums.length; i++) {
+        remainder = (remainder + nums[i]) % k;
+        if (map.has(remainder)) {
+            if (i - map.get(remainder) >= 2) return true;
+        } else {
+            map.set(remainder, i);
+        }
     }
     return false;
 };
