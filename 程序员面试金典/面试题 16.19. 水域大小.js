58237b59260b488752afc820bf58677df398eef1
JX-Zhuang <who.zhuang@gmail.com>
2021-07-15 22:00:10 +0800

    pondSizes

--- 程序员面试金典/面试题 16.19. 水域大小.js
+++ 程序员面试金典/面试题 16.19. 水域大小.js
@@ -2,6 +2,32 @@
  * @param {number[][]} land
  * @return {number[]}
  */
-var pondSizes = function(land) {
-
+var pondSizes = function (land) {
+    var ans = [];
+    var n = land.length, m = land[0].length;
+    var count = 0;
+    var dfs = function (x, y) {
+        if (x < 0 || y < 0 || x >= n || y >= m || land[x][y] !== 0) return;
+        land[x][y] = -1;
+        count++;
+        dfs(x - 1, y);
+        dfs(x + 1, y);
+        dfs(x, y - 1);
+        dfs(x, y + 1);
+        dfs(x + 1, y + 1);
+        dfs(x + 1, y - 1);
+        dfs(x - 1, y + 1);
+        dfs(x - 1, y - 1);
+    };
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < m; j++) {
+            if (land[i][j] === 0) {
+                count = 0;
+                dfs(i, j);
+                ans.push(count);
+            }
+        }
+    }
+    ans.sort((a, b) => a - b);
+    return ans;
 };
