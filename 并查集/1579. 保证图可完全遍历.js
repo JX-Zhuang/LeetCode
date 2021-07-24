9b04e209310321caa40e2b60cec5a3c174da9232
JX-Zhuang <who.zhuang@gmail.com>
2021-07-24 23:24:28 +0800

    maxNumEdgesToRemove

--- 并查集/1579. 保证图可完全遍历.js
+++ 并查集/1579. 保证图可完全遍历.js
@@ -3,6 +3,23 @@
  * @param {number[][]} edges
  * @return {number}
  */
- var maxNumEdgesToRemove = function(n, edges) {
+var maxNumEdgesToRemove = function (n, edges) {
 
 };
+class UnionFind {
+    constructor(n) {
+        this.parent = Array.from(new Array(n), (item, index) => index);
+    }
+    find(x) {
+        while (x !== this.parent[x]) {
+            this.parent[x] = this.parent[this.parent[x]];
+            x = this.parent[x];
+        }
+        return x;
+    }
+    union(x, y) {
+        var a = this.find(x), b = this.find(y);
+        if (a === b) return;
+        this.parent[a] = b;
+    }
+}
