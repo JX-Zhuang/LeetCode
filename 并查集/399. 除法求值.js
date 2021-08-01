3278a1428099e070f3c3e41a4fc152663dfbf248
JX-Zhuang <who.zhuang@gmail.com>
2021-08-01 23:01:36 +0800

    calcEquation

--- 并查集/399. 除法求值.js
+++ 并查集/399. 除法求值.js
@@ -4,6 +4,23 @@
  * @param {string[][]} queries
  * @return {number[]}
  */
- var calcEquation = function(equations, values, queries) {
+var calcEquation = function (equations, values, queries) {
 
 };
+class UnionFind {
+    constructor(n) {
+        this.parents = Array.from(new Array(n).fill(0), (item, index) => index);
+    }
+    find(a) {
+        while (this.parents[a] !== a) {
+            this.parents[a] = this.parents[this.parents[n]];
+            a = this.parents[a];
+        }
+        return a;
+    }
+    union(a, b) {
+        const x = this.parents[a], y = this.parents[b];
+        if (x === y) return;
+        this.parents[x] = y;
+    }
+}
