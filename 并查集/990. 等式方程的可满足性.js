aac28e0c6ea4e83f2c7584bcbb3770944754b1ee
JX-Zhuang <who.zhuang@gmail.com>
2021-08-02 21:54:01 +0800

    equationsPossible

--- 并查集/990. 等式方程的可满足性.js
+++ 并查集/990. 等式方程的可满足性.js
@@ -2,6 +2,37 @@
  * @param {string[]} equations
  * @return {boolean}
  */
-var equationsPossible = function(equations) {
-
+var index = function (a) {
+    return a.charCodeAt() - 97;
+}
+var equationsPossible = function (equations) {
+    var uf = new UnionFind(26);
+    for (const item of equations) {
+        if (item[1] == '=') {
+            uf.union(index(item[0]), index(item[3]));
+        }
+    }
+    for (const item of equations) {
+        if (item[1] == '!') {
+            if (uf.find(index(item[0])) === uf.find(index(item[3]))) return false;
+        }
+    }
+    return true;
 };
+class UnionFind {
+    constructor(n) {
+        this.parents = Array.from(new Array(n).fill(0), (item, index) => index);
+    }
+    find(n) {
+        while (n !== this.parents[n]) {
+            this.parents[n] = this.parents[this.parents[n]];
+            n = this.parents[n];
+        }
+        return n;
+    }
+    union(a, b) {
+        const x = this.find(a), y = this.find(b);
+        if (x === y) return;
+        this.parents[x] = y;
+    }
+}
