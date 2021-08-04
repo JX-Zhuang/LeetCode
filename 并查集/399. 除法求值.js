04ca8534ccb41becf4a086dde44b90feb568c500
JX-Zhuang <who.zhuang@gmail.com>
2021-08-04 23:21:54 +0800

    equationsPossiblecalcEquation

--- 并查集/399. 除法求值.js
+++ 并查集/399. 除法求值.js
@@ -8,22 +8,54 @@ var calcEquation = function (equations, values, queries) {
     var l = equations.length;
     var uf = new UnionFind(2 * l);
     var m = new Map();
-    
+    var id = 0;
+    for (var i = 0; i < equations.length; i++) {
+        var item = equations[i];
+        var a = item[0], b = item[1];
+        if (!m.has(a)) {
+            m.set(a, id++);
+        }
+        if (!m.has(b)) {
+            m.set(b, id++);
+        }
+        var idA = m.get(a), idB = m.get(b);
+        uf.union(idA, idB, values[i]);
+    }
+    var result = [];
+    for (var i = 0; i < queries.length; i++) {
+        var item = queries[i];
+        var a = item[0], b = item[1];
+        var idA = m.get(a), idB = m.get(b);
+        if (idA === undefined || idB === undefined) {
+            result[i] = -1;
+        } else {
+            result[i] = uf.isConnected(idA, idB);
+        }
+    }
+    return result;
 };
 class UnionFind {
     constructor(n) {
         this.parents = Array.from(new Array(n).fill(0), (item, index) => index);
+        this.weights = new Array(n).fill(1);
     }
-    find(a) {
-        while (this.parents[a] !== a) {
-            this.parents[a] = this.parents[this.parents[n]];
-            a = this.parents[a];
+    find(a){
+        if(a!==this.parents[a]){
+            var origin = this.parents[a];
+            this.parents[a] = this.find(this.parents[a]);
+            this.weights[a]*=this.weights[origin];
         }
-        return a;
+        return this.parents[a];
     }
-    union(a, b) {
-        const x = this.parents[a], y = this.parents[b];
+    union(a, b, value) {
+        const x = this.find(a), y = this.find(b);
         if (x === y) return;
         this.parents[x] = y;
+        this.weights[x] = this.weights[b] * value / this.weights[a];
+    }
+    isConnected(a, b) {
+        var x = this.find(a), y = this.find(b);
+        if (x === y) return this.weights[a] / this.weights[b];
+        return -1;
     }
 }
