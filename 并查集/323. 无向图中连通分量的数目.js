f875be8ab12d1b303457850d4c19c21bc2c4b37a
JX-Zhuang <who.zhuang@gmail.com>
2021-08-06 23:31:51 +0800

    countComponents

--- 并查集/323. 无向图中连通分量的数目.js
+++ 并查集/323. 无向图中连通分量的数目.js
@@ -3,6 +3,23 @@
  * @param {number[][]} edges
  * @return {number}
  */
- var countComponents = function(n, edges) {
+var countComponents = function (n, edges) {
 
 };
+class UnionFind {
+    constructor(n) {
+        this.parents = Array.from(new Array(n).fill(0), (_, index) => index);
+    }
+    find(a) {
+        while (a !== this.parents[a]) {
+            this.parents[a] = this.parents[this.parents[a]];
+            a = this.parents[a];
+        }
+        return a;
+    }
+    union(a, b) {
+        const x = this.find(a), y = this.find(b);
+        if (x === y) return;
+        this.parents[x] = y; z
+    }
+}
