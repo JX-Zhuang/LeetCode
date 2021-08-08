d9aea59d0180ba1b3d06a3d97573cc3e60a27c4c
JX-Zhuang <who.zhuang@gmail.com>
2021-08-08 22:40:59 +0800

    validTree

--- 并查集/261. 以图判树.js
+++ 并查集/261. 以图判树.js
@@ -3,6 +3,32 @@
  * @param {number[][]} edges
  * @return {boolean}
  */
- var validTree = function(n, edges) {
-
+var validTree = function (n, edges) {
+    const uf = new UnionFind(n);
+    for (const edge of edges) {
+        if (!uf.union(...edge)) return false;
+    }
+    return uf.getCount() === 1;
 };
+class UnionFind {
+    constructor(n) {
+        this.count = n;
+        this.parents = Array.from(new Array(n), (_, index) => index);
+    }
+    find(a) {
+        if (a !== this.parents[a]) {
+            this.parents[a] = this.find(this.parents[a]);
+        }
+        return this.parents[a];
+    }
+    union(a, b) {
+        const x = this.find(a), y = this.find(b);
+        if (x === y) return false;
+        this.count--;
+        this.parents[x] = y;
+        return true;
+    }
+    getCount() {
+        return this.count;
+    }
+}
