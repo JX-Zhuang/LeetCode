ff345d61d0d02eedc36fd08fd14a675a748377ab
JX-Zhuang <who.zhuang@gmail.com>
2021-08-10 22:48:09 +0800

    numEnclaves

--- 并查集/1020. 飞地的数量.js
+++ 并查集/1020. 飞地的数量.js
@@ -2,6 +2,19 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var numEnclaves = function(grid) {
-
+var numEnclaves = function (grid) {
+    var l1 = grid.length, l2 = grid[0].length;
+    var dfs = function (i, j) {
+        if (grid[i][j] === 0) return false;
+        if (i < 0 || j < 0 || i >= l1 || j >= l2) return false;
+        grid[i][j] = 0;
+        return true && dfs(i - 1, j) && dfs(i + 1, j) && dfs(i, j + 1) && dfs(i, j - 1);
+    };
+    var result = 0;
+    for (var i = 0; i < l1; i++) {
+        for (var j = 0; j < l2; j++) {
+            if (dfs(i, j)) result++;
+        }
+    }
+    return result;
 };
