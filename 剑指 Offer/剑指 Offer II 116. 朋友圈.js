aaf5b336c2bdb85d0368ecc886081e7c7439b2d3
JX-Zhuang <who.zhuang@gmail.com>
2021-08-13 23:02:20 +0800

    findCircleNum

--- 剑指 Offer/剑指 Offer II 116. 朋友圈.js
+++ 剑指 Offer/剑指 Offer II 116. 朋友圈.js
@@ -2,6 +2,39 @@
  * @param {number[][]} isConnected
  * @return {number}
  */
- var findCircleNum = function(isConnected) {
-
+var findCircleNum = function (isConnected) {
+    var uf = new UnionFind(isConnected.length);
+    for (var i = 0; i < isConnected.length; i++) {
+        for (var j = 0; j < isConnected[i].length; j++) {
+            if (isConnected[i][j]) {
+                uf.union(i, j);
+            }
+        }
+    }
+    return uf.getCount();
 };
+class UnionFind {
+    constructor(n) {
+        this.count = n;
+        this.parents = Array.from(new Array(n), (_, index) => index);
+    }
+    find(a) {
+        if (a !== this.parents[a]) {
+            this.parents[a] = this.find(this.parents[a]);
+        }
+        return this.parents[a];
+    }
+    union(a, b) {
+        const x = this.find(a), y = this.find(b);
+        if (x === y) return false;
+        this.count--;
+        this.parents[x] = y;
+        return true;
+    }
+    getCount() {
+        return this.count;
+    }
+    isConnected(a, b) {
+        return this.find(a) === this.find(b);
+    }
+}
