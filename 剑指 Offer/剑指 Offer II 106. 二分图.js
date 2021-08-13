a841f0e5a8fdde064748f0511bf7f8013501947f
JX-Zhuang <who.zhuang@gmail.com>
2021-08-13 22:23:15 +0800

    isBipartite

--- 剑指 Offer/剑指 Offer II 106. 二分图.js
+++ 剑指 Offer/剑指 Offer II 106. 二分图.js
@@ -2,6 +2,28 @@
  * @param {number[][]} graph
  * @return {boolean}
  */
- var isBipartite = function(graph) {
+var isBipartite = function (graph) {
 
 };
+class UnionFind {
+    constructor(n) {
+        this.count = n;
+        this.parents = Array.from(new Array(n), (_, index) => index);
+    }
+    find(a) {
+        if (a !== this.parents[a]) {
+            this.parents[a] = this.find(this.parents[a]);
+        }
+        return this.parents[a];
+    }
+    union(a, b) {
+        const x = this.find(a), y = this.find(b);
+        if (x === y) return false;
+        this.count--;
+        this.parents[x] = y;
+        return true;
+    }
+    getCount() {
+        return this.count;
+    }
+}
