c7dc65d12d02db440f715cb30d7b97bd2afea3a9
JX-Zhuang <who.zhuang@gmail.com>
2021-08-21 23:50:32 +0800

    update

--- 剑指 Offer/剑指 Offer 29. 顺时针打印矩阵.js
+++ 剑指 Offer/剑指 Offer 29. 顺时针打印矩阵.js
@@ -7,30 +7,29 @@ var spiralOrder = function (matrix) {
     var rows = matrix.length;
     if (rows == 0) return result;
     var cols = matrix[0].length;
-    var visited = Array.from(new Array(rows), () => new Array(cols).fill(false));
     var printNumber = function (x, y) {
-        if (x < 0 || y < 0 || x >= rows || y >= cols || visited[x][y]) return;
+        if (x < 0 || y < 0 || x >= rows || y >= cols || matrix[x][y] === true) return;
         var lX = rows - x,
             lY = cols - y;
         for (var i = y; i <= lY - 1; i++) {
-            if (visited[x][i]) continue;
+            if (matrix[x][i] === true) continue;
             result.push(matrix[x][i]);
-            visited[x][i] = true;
+            matrix[x][i] = true;
         }
         for (var j = x; j <= lX - 1; j++) {
-            if (visited[j][lY - 1]) continue;
+            if (matrix[j][lY - 1] === true) continue;
             result.push(matrix[j][lY - 1]);
-            visited[j][lY - 1] = true;
+            matrix[j][lY - 1] = true;
         }
         for (var k = lY - 1; k >= y; k--) {
-            if (visited[lX - 1][k]) continue;
+            if (matrix[lX - 1][k] === true) continue;
             result.push(matrix[lX - 1][k]);
-            visited[lX - 1][k] = true;
+            matrix[lX - 1][k] = true;
         }
         for (var h = lX - 1; h >= x; h--) {
-            if (visited[h][y]) continue;
+            if (matrix[h][y] === true) continue;
             result.push(matrix[h][y]);
-            visited[h][y] = true;
+            matrix[h][y] = true;
         }
         printNumber(x + 1, y + 1);
     }
