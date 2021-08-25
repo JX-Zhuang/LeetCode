4aa47d98491dc549f26b30a0c1c939087cba2ff6
JX-Zhuang <who.zhuang@gmail.com>
2021-08-25 23:22:24 +0800

    treeToDoublyList

--- 剑指 Offer/剑指 Offer 36. 二叉搜索树与双向链表.js
+++ 剑指 Offer/剑指 Offer 36. 二叉搜索树与双向链表.js
@@ -10,6 +10,23 @@
  * @param {Node} root
  * @return {Node}
  */
-var treeToDoublyList = function(root) {
-
+var treeToDoublyList = function (root) {
+    var head, prev;
+    if (!root) return root;
+    var helper = function (root) {
+        if (!root) return;
+        helper(root.left);
+        if (head) {
+            prev.right = root;
+            root.left = prev;
+        } else {
+            head = root;
+        }
+        prev = root;
+        helper(root.right);
+    }
+    helper(root);
+    prev.right = head;
+    head.left = prev;
+    return head;
 };
