81918536bf4eac56d8b872eff1c0d5d880295e4c
JX-Zhuang <who.zhuang@gmail.com>
2021-08-28 00:55:23 +0800

    update

--- 剑指 Offer/剑指 Offer 56 - I. 数组中数字出现的次数.js
+++ 剑指 Offer/剑指 Offer 56 - I. 数组中数字出现的次数.js
@@ -2,6 +2,22 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var singleNumbers = function(nums) {
-
+ var singleNumbers = function (nums) {
+    var ret = 0;
+    for (var i of nums) {
+        ret ^= i;
+    }
+    var div = 1;
+    while ((div & ret) == 0) {
+        div <<= 1;
+    }
+    var a = 0, b = 0;
+    for (var i of nums) {
+        if ((div & i) === 0) {
+            a ^= i;
+        } else {
+            b ^= i;
+        }
+    }
+    return [a,b];
 };
