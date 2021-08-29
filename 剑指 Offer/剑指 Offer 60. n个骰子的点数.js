9fd9c98922ca0024be11313d2c06f79684f9634a
JX-Zhuang <who.zhuang@gmail.com>
2021-08-29 22:20:16 +0800

    dicesProbability

--- 剑指 Offer/剑指 Offer 60. n个骰子的点数.js
+++ 剑指 Offer/剑指 Offer 60. n个骰子的点数.js
@@ -2,6 +2,16 @@
  * @param {number} n
  * @return {number[]}
  */
- var dicesProbability = function(n) {
-
+var dicesProbability = function (n) {
+    var result = new Array(6).fill(1 / 6);
+    for (var i = 2; i <= n; i++) {
+        var arr = new Array(5 * i + 1).fill(0);
+        for (var j = 0; j < result.length; j++) {
+            for (var k = 0; k < 6; k++) {
+                arr[j + k] += result[j] / 6;
+            }
+        }
+        result = arr;
+    }
+    return result;
 };
