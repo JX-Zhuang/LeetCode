da8c94df50e02585da76d9cacd971140796bd513
JX-Zhuang <who.zhuang@gmail.com>
2021-08-30 22:56:35 +0800

    copyRandomList

--- 剑指 Offer/剑指 Offer 35. 复杂链表的复制.js
+++ 剑指 Offer/剑指 Offer 35. 复杂链表的复制.js
@@ -11,6 +11,22 @@
  * @param {Node} head
  * @return {Node}
  */
-var copyRandomList = function(head) {
-    
+var copyRandomList = function (head) {
+    var m = new Map();
+    var node = head;
+    var newHead = new Node(), prev = newHead;
+    while (node) {
+        prev.next = new Node(node.val, null, null);
+        m.set(node, prev.next);
+        prev = prev.next;
+        node = node.next;
+    }
+    node = head;
+    prev = newHead.next;
+    while (node) {
+        prev.random = m.get(node.random);
+        node = node.next;
+        prev = prev.next;
+    }
+    return newHead.next;
 };
