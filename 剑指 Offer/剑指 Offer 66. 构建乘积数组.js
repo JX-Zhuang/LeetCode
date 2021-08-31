c56a7edcbb867e9ef2159fada18e6bfdac4cb938
JX-Zhuang <who.zhuang@gmail.com>
2021-08-31 20:03:06 +0800

    constructArr

--- 剑指 Offer/剑指 Offer 66. 构建乘积数组.js
+++ 剑指 Offer/剑指 Offer 66. 构建乘积数组.js
@@ -2,6 +2,15 @@
  * @param {number[]} a
  * @return {number[]}
  */
- var constructArr = function(a) {
-
+var constructArr = function (a) {
+    var result = new Array(a.length).fill(1);
+    for (var i = 1; i < a.length; i++) {
+        result[i] = result[i - 1] * a[i - 1];
+    }
+    var temp = 1;
+    for (var i = a.length - 2; i >= 0; i--) {
+        temp *= a[i + 1];
+        result[i] *= temp;
+    }
+    return result;
 };
