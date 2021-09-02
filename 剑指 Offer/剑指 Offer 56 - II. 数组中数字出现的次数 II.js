9936c43178a8f5178d9bfe689ce232ade2e63617
JX-Zhuang <who.zhuang@gmail.com>
2021-09-02 21:59:04 +0800

    singleNumber

--- 剑指 Offer/剑指 Offer 56 - II. 数组中数字出现的次数 II.js
+++ 剑指 Offer/剑指 Offer 56 - II. 数组中数字出现的次数 II.js
@@ -2,6 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-
+var singleNumber = function (nums) {
+    var m = {};
+    for (const n of nums) {
+        if (!m.hasOwnProperty(n)) {
+            m[n] = 0;
+        }
+        m[n]++;
+    }
+    for (const k in m) {
+        if (m[k] === 1) return k
+    }
 };
