35404e871d1f393569045082e4ccfb799bbe3370
JX-Zhuang <who.zhuang@gmail.com>
2021-09-04 23:16:09 +0800

    myAtoi

--- 字符串/8. 字符串转换整数 (atoi).js
+++ 字符串/8. 字符串转换整数 (atoi).js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var strToInt = function (str) {
+var myAtoi = function (str) {
     var result = '';
     for (var i = 0; i < str.length; i++) {
         var s = str[i];
