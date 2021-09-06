0643ce96c725e831a0b153db819fa17394170e42
JX-Zhuang <who.zhuang@gmail.com>
2021-09-06 22:56:23 +0800

    add

--- 剑指 Offer/剑指 Offer 65. 不用加减乘除做加法.js
+++ 剑指 Offer/剑指 Offer 65. 不用加减乘除做加法.js
@@ -3,6 +3,24 @@
  * @param {number} b
  * @return {number}
  */
-var add = function(a, b) {
-
+var add = function (a, b) {
+    while (b !== 0) {
+        const c = (a & b) << 1;
+        a ^= b;
+        b = c;
+    }
+    return a;
 };
+/**
+ * 2+3=5
+ * 10+11=101
+ * a b c d
+ * 0 0 0 0
+ * 1 0 1 0
+ * 0 1 1 0
+ * 1 1 0 1
+ * a^b = c
+ * a&b = d
+ * if(d==0) c
+ * else d<<1+c
+ */
