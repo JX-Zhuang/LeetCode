f342861e2b20890d6361e2b217f2b6c64e6bab97
JX-Zhuang <who.zhuang@gmail.com>
2021-09-07 19:36:12 +0800

    myPow

--- 剑指 Offer/剑指 Offer 16. 数值的整数次方.js
+++ 剑指 Offer/剑指 Offer 16. 数值的整数次方.js
@@ -8,7 +8,8 @@ var myPow = function (x, n) {
         if (n === 0) return 1;
         if (n === 1) return x;
         var m = Math.floor(n / 2);
-        return helper(x, m) * helper(x, m) * (n % 2 ? x : 1);
+        var r = helper(x, m);
+        return  r * r * (n % 2 ? x : 1);
     }
     return n < 0 ? 1 / helper(x, -1 * n) : helper(x, n);
 };
