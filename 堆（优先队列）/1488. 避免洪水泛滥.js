fca4a75bc90edd7b98ee8b79f0e8a27555f1e434
JX-Zhuang <who.zhuang@gmail.com>
2021-09-11 23:51:53 +0800

    avoidFlood

--- 堆（优先队列）/1488. 避免洪水泛滥.js
+++ 堆（优先队列）/1488. 避免洪水泛滥.js
@@ -2,6 +2,30 @@
  * @param {number[]} rains
  * @return {number[]}
  */
-var avoidFlood = function(rains) {
-
+var avoidFlood = function (rains) {
+    var result = new Array(rains.length).fill(1);
+    var zero = new Set();
+    var water = {};
+    for (var i = 0; i < rains.length; i++) {
+        var item = rains[i];
+        if (item === 0) {
+            zero.add(i);
+            continue;
+        }
+        if (water.hasOwnProperty(item)) {
+            var find = false;
+            for (var j of zero) {
+                if (j > water[item]) {
+                    find = true;
+                    break;
+                }
+            }
+            if (!find) return [];
+            result[j] = item;
+            zero.delete(j);
+        }
+        result[i] = -1;
+        water[item] = i;
+    }
+    return result;
 };
