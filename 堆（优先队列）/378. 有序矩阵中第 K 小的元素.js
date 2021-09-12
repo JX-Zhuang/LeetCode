95e28ca5e6c58e38ddbbe6e1f31a8102a8d66d97
JX-Zhuang <who.zhuang@gmail.com>
2021-09-12 22:59:28 +0800

    kthSmallest

--- 堆（优先队列）/378. 有序矩阵中第 K 小的元素.js
+++ 堆（优先队列）/378. 有序矩阵中第 K 小的元素.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 class PriorityQueue {
-    constructor(n) {
-        this.queue = new Array(n + 1);
+    constructor() {
+        this.queue = new Array(null);
         this.n = 0;
     }
+    comparator(a, b) {
+        return this.queue[a][0] > this.queue[b][0];
+    };
     exch(a, b) {
         const temp = this.queue[a];
         this.queue[a] = this.queue[b];
         this.queue[b] = temp;
-    }
-    less(a, b) {
-        return this.queue[a] < this.queue[b];
     }
     size() {
         return this.n;
@@ -24,42 +24,42 @@ class PriorityQueue {
         this.swim(this.n);
     }
     swim(n) {
-        while (n > 1 && this.less(n >> 1, n)) {
+        while (n > 1 && this.comparator(n >> 1, n)) {
             this.exch(n >> 1, n);
             n = n >> 1;
         }
     }
     sink(n) {
-        while (n * 2 < this.n) {
+        while (n * 2 <= this.n) {
             let i = n * 2;
-            if (i < this.n && this.less(i, i + 1)) i++;
-            if (this.less(i, n)) break;
+            if (i < this.n && this.comparator(i, i + 1)) i++;
+            if (this.comparator(i, n)) break;
             this.exch(i, n);
             n = i;
         }
     }
-    deleteMax() {
-        this.exch(1, this.n--);
+    poll() {
+        const result = this.queue[1];
+        // this.exch(1, this.n--);
+        this.queue[1] = this.queue[this.n--];
         this.sink(1);
-        delete this.queue[this.n + 1];
+        return result;
     }
     peek() {
         return this.queue[1];
     }
 }
 var kthSmallest = function (matrix, k) {
-    var maxPQ = new PriorityQueue(k);
-    for (var item of matrix) {
-        for (var num of item) {
-            if (maxPQ.size() !== k) {
-                maxPQ.insert(num);
-                continue;
-            }
-            if (num < maxPQ.peek()) {
-                maxPQ.deleteMax();
-                maxPQ.insert(num);
-            }
+    var pq = new PriorityQueue();
+    var n = matrix.length;
+    for (var i = 0; i < n; i++) {
+        pq.insert([matrix[i][0], i, 0]);
+    }
+    for (var i = 0; i < k - 1; i++) {
+        const now = pq.poll();
+        if (now[2] !== n - 1) {
+            pq.insert([matrix[now[1]][now[2] + 1], now[1], now[2] + 1]);
         }
     }
-    return maxPQ.peek();
+    return pq.poll()[0];
 };
