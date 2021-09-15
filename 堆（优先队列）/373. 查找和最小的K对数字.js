c0c970e21f7f7b84f003793f04a1f89c565715bd
JX-Zhuang <who.zhuang@gmail.com>
2021-09-15 22:41:20 +0800

    kSmallestPairs

--- 堆（优先队列）/373. 查找和最小的K对数字.js
+++ 堆（优先队列）/373. 查找和最小的K对数字.js
@@ -4,6 +4,67 @@
  * @param {number} k
  * @return {number[][]}
  */
-var kSmallestPairs = function(nums1, nums2, k) {
-
+class PriorityQueue {
+    constructor() {
+        this.pq = [null];
+        this.n = 0;
+    }
+    exch(a, b) {
+        [this.pq[a], this.pq[b]] = [this.pq[b], this.pq[a]];
+    }
+    comparator(a, b) {
+        const itemA = this.pq[a], itemB = this.pq[b];
+        return itemA[1] < itemB[1];
+    }
+    insert(value) {
+        this.pq[++this.n] = value;
+        this.swim(this.n);
+    }
+    swim(n) {
+        while (n > 1 && this.comparator(n >> 1, n)) {
+            this.exch(n >> 1, n);
+            n >>= 1;
+        }
+    }
+    poll() {
+        const result = this.pq[1];
+        this.pq[1] = this.pq[this.n--];
+        this.sink(1);
+        delete this.pq[this.n + 1];
+        return result;
+    }
+    sink(n) {
+        while (n * 2 <= this.n) {
+            let i = n * 2;
+            if (i < this.n && this.comparator(i, i + 1)) i++;
+            if (this.comparator(i, n)) break;
+            this.exch(i, n);
+            n = i;
+        }
+    }
+    peek() {
+        return this.pq[1];
+    }
+    size() {
+        return this.n;
+    }
+}
+var kSmallestPairs = function (nums1, nums2, k) {
+    const maxPQ = new PriorityQueue();
+    for (let i = 0; i < Math.min(nums1.length, k); i++) {
+        const n1 = nums1[i];
+        for (let j = 0; j < Math.min(nums2.length, k); j++) {
+            const n2 = nums2[j];
+            const sum = n1 + n2;
+            if (maxPQ.size() < k) {
+                maxPQ.insert([[n1, n2], sum]);
+                continue;
+            }
+            if (sum < maxPQ.peek()[1]) {
+                maxPQ.poll();
+                maxPQ.insert([[n1, n2], sum]);
+            }
+        }
+    }
+    return maxPQ.pq.slice(1).map(item => item[0]);
 };
