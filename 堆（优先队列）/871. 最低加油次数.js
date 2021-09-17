0fcb576533c6605baa8fb3065629f1ff95918675
JX-Zhuang <who.zhuang@gmail.com>
2021-09-17 22:58:46 +0800

    minRefuelStops

--- 堆（优先队列）/871. 最低加油次数.js
+++ 堆（优先队列）/871. 最低加油次数.js
@@ -4,6 +4,19 @@
  * @param {number[][]} stations
  * @return {number}
  */
- var minRefuelStops = function(target, startFuel, stations) {
-
+var minRefuelStops = function (target, startFuel, stations) {
+    var n = stations.length;
+    var dp = new Array(n).fill(0);
+    dp[0] = startFuel;
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j <= i; j++) {
+            if (dp[i] >= stations[i][0]) {
+                dp[i + 1] = Math.max(dp[i], stations[i][1] + dp[i]);
+            }
+        }
+    }
+    for (var i = 0; i <= n; i++) {
+        if (dp[i] >= target) return i;
+    }
+    return -1;
 };
