27f3aa1ae12f71600e7dff3032e9aaaecf554e7c
JX-Zhuang <who.zhuang@gmail.com>
2021-09-17 23:57:40 +0800

    minRefuelStops

--- 堆（优先队列）/871. 最低加油次数.js
+++ 堆（优先队列）/871. 最低加油次数.js
@@ -20,3 +20,65 @@ var minRefuelStops = function (target, startFuel, stations) {
     }
     return -1;
 };
+class PriorityQueue {
+    constructor() {
+        this.pq = [null];
+        this.n = 0;
+    }
+    exch(a, b) {
+        [this.pq[a], this.pq[b]] = [this.pq[b], this.pq[a]];
+    }
+    comparator(a, b) {
+        const itemA = this.pq[a], itemB = this.pq[b];
+        return itemA < itemB;
+    }
+    insert(value) {
+        this.pq[++this.n] = value;
+        this.swim(this.n);
+    }
+    swim(n) {
+        while (n > 1 && this.comparator(n >> 1, n)) {
+            this.exch(n >> 1, n);
+            n >>= 1;
+        }
+    }
+    poll() {
+        const result = this.pq[1];
+        this.pq[1] = this.pq[this.n--];
+        this.sink(1);
+        delete this.pq[this.n + 1];
+        return result;
+    }
+    sink(n) {
+        while (n * 2 <= this.n) {
+            let i = n * 2;
+            if (i < this.n && this.comparator(i, i + 1)) i++;
+            if (this.comparator(i, n)) break;
+            this.exch(i, n);
+            n = i;
+        }
+    }
+    peek() {
+        return this.pq[1];
+    }
+    size() {
+        return this.n;
+    }
+}
+var minRefuelStops = function (target, startFuel, stations) {
+    stations.push([target, 0]);
+    const maxPQ = new PriorityQueue();
+    let result = 0, prev = 0, tank = startFuel;
+    for (const item of stations) {
+        const [location, fuel] = item;
+        tank = tank - location + prev;
+        while (maxPQ.size() && tank < 0) {
+            tank += maxPQ.poll();
+            result++;
+        }
+        if (tank < 0) return -1;
+        maxPQ.insert(fuel);
+        prev = location;
+    }
+    return result;
+}
