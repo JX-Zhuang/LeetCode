c4ca18d15ae3db3fb5d3c66269943c32314be029
JX-Zhuang <who.zhuang@gmail.com>
2021-09-18 22:37:49 +0800

    kWeakestRows

--- 堆（优先队列）/1337. 矩阵中战斗力最弱的 K 行.js
+++ 堆（优先队列）/1337. 矩阵中战斗力最弱的 K 行.js
@@ -3,6 +3,10 @@
  * @param {number} k
  * @return {number[]}
  */
- var kWeakestRows = function(mat, k) {
-
+var kWeakestRows = function (mat, k) {
+    var arr = mat.map((item, index) => ({
+        index,
+        count: item.reduce((a, b) => a + b, 0)
+    }));
+    return arr.sort((a, b) => a.count - b.count).slice(0, k).map(a => a.index);
 };
