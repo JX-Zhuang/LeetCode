48512e3a312964e4e6ccee4f5d0e2399c6c9e8c8
JX-Zhuang <who.zhuang@gmail.com>
2021-09-19 23:17:35 +0800

    findCheapestPrice

--- 堆（优先队列）/787. K 站中转内最便宜的航班.js
+++ 堆（优先队列）/787. K 站中转内最便宜的航班.js
@@ -6,6 +6,18 @@
  * @param {number} k
  * @return {number}
  */
- var findCheapestPrice = function(n, flights, src, dst, k) {
-
+var findCheapestPrice = function (n, flights, src, dst, k) {
+    const INF = 10000 * 101 + 1;
+    const dp = new Array(k + 2).fill(0).map(() => new Array(n).fill(INF));
+    dp[0][src] = 0;
+    for (let t = 1; t <= k + 1; t++) {
+        for (const [i, j, cost] of flights) {
+            dp[t][j] = Math.min(dp[t][j], dp[t - 1][i] + cost);
+        }
+    }
+    let ans = INF;
+    for (let t = 1; t <= k + 1; t++) {
+        ans = Math.min(ans, dp[t][dst]);
+    }
+    return ans === INF ? -1 : ans;
 };
