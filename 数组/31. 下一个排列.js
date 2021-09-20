f2e9a8389903ae28df3b9bd3985808eb9f3b8bf4
JX-Zhuang <who.zhuang@gmail.com>
2021-09-20 22:55:27 +0800

    nextPermutation

--- 数组/31. 下一个排列.js
+++ 数组/31. 下一个排列.js
@@ -2,6 +2,22 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- var nextPermutation = function(nums) {
-
+var reverse = function (nums, i) {
+    var left = i, right = nums.length - 1;
+    while (left < right) {
+        swap(nums, left++, right--);
+    }
+}
+var swap = function (nums, i, j) {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+}
+var nextPermutation = function (nums) {
+    var i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+    if (i >= 0) {
+        var j = nums.length - 1;
+        while (j >= 0 && nums[i] >= nums[j]) j--;
+        swap(nums, i, j);
+    }
+    reverse(nums, i + 1);
 };
