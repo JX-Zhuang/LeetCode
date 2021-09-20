407a64b7901051c1479240d7de1a7809fa274af1
JX-Zhuang <who.zhuang@gmail.com>
2021-09-21 00:55:17 +0800

    canJump

--- 贪心算法/55. 跳跃游戏.js
+++ 贪心算法/55. 跳跃游戏.js
@@ -8,6 +8,6 @@ var canJump = function (nums) {
     for (var i = 0; i < n; i++) {
         if (i > max) return false;
         max = Math.max(max, i + nums[i]);
-        if (max >= n - 1) return true;
     }
+    return true;
 };
