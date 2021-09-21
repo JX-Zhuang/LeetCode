b39c195c5de769641531b6a908cc5f90d1a04e66
JX-Zhuang <who.zhuang@gmail.com>
2021-09-21 21:32:25 +0800

    rotate

--- 数组/48. 旋转图像.js
+++ 数组/48. 旋转图像.js
@@ -2,6 +2,16 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
- var rotate = function(matrix) {
-
+var rotate = function (matrix) {
+    var n = matrix.length;
+    for (var i = 0; i < n >> 1; i++) {
+        for (var j = 0; j < n; j++) {
+            [matrix[i][j], matrix[n - i - 1][j]] = [matrix[n - i - 1][j], matrix[i][j]];
+        }
+    }
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < i; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
 };
