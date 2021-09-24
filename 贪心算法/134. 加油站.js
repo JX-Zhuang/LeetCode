b9c72b95971b457dc5bbdb4b6b281362a2169e9d
JX-Zhuang <who.zhuang@gmail.com>
2021-09-24 22:19:20 +0800

    canCompleteCircuit

--- 贪心算法/134. 加油站.js
+++ 贪心算法/134. 加油站.js
@@ -3,6 +3,19 @@
  * @param {number[]} cost
  * @return {number}
  */
- var canCompleteCircuit = function(gas, cost) {
-
+var canCompleteCircuit = function (gas, cost) {
+    const len = gas.length;
+    var minIndex = 0, minSpare = Number.MAX_SAFE_INTEGER, spare = 0;
+    for (var i = 0; i < len; i++) {
+        spare += gas[i] - cost[i];
+        if (spare < minSpare) {
+            minSpare = spare;
+            minIndex = i;
+        }
+    }
+    if (spare < 0) return -1;
+    if (minIndex === 0 && gas[minIndex] >= cost[minIndex]) {
+        return minIndex;
+    }
+    return (minIndex + 1) % len;
 };
