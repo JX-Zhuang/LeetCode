36423d9fdc2fedf800f9d2f2036b7da04aa2a922
JX-Zhuang <who.zhuang@gmail.com>
2021-09-26 22:35:24 +0800

    shuffle-an-array

--- 数组/384. 打乱数组.js
+++ 数组/384. 打乱数组.js
@@ -1,26 +1,33 @@
 /**
  * @param {number[]} nums
  */
-var Solution = function(nums) {
-
+var Solution = function (nums) {
+    this.arr = nums;
 };
 
 /**
  * Resets the array to its original configuration and return it.
  * @return {number[]}
  */
-Solution.prototype.reset = function() {
-
+Solution.prototype.reset = function () {
+    return this.arr;
 };
-
+var getIndex = (min, max) => Math.floor(Math.random() * (max - min) + min);
 /**
  * Returns a random shuffling of the array.
  * @return {number[]}
  */
-Solution.prototype.shuffle = function() {
-
+Solution.prototype.shuffle = function () {
+    var copy = [...this.arr];
+    for (var i = 0; i < copy.length; i++) {
+        const index = getIndex(i, copy.length);
+        this.exch(i, index, copy);
+    }
+    return copy;
 };
-
+Solution.prototype.exch = function (a, b, copy) {
+    [copy[a], copy[b]] = [copy[b], copy[a]];
+}
 /**
  * Your Solution object will be instantiated and called as such:
  * var obj = new Solution(nums)
