7253a2e0584ebde5e542e8ba83ba90c305f6b28f
JX-Zhuang <who.zhuang@gmail.com>
2021-09-27 21:45:18 +0800

    getSum

--- 数学/371. 两整数之和.js
+++ 数学/371. 两整数之和.js
@@ -3,6 +3,11 @@
  * @param {number} b
  * @return {number}
  */
-var getSum = function(a, b) {
-
+var getSum = function (a, b) {
+    while (b) {
+        const c = (a & b) << 1;
+        a = a ^ b;
+        b = c;
+    }
+    return a;
 };
