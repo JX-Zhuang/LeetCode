ddb84714d3ee810d7f4f2dd95e8d226ed5b0b7e4
JX-Zhuang <who.zhuang@gmail.com>
2021-11-14 22:21:30 +0800

    groupAnagrams

--- 程序员面试金典/面试题 10.02. 变位词组.js
+++ 程序员面试金典/面试题 10.02. 变位词组.js
@@ -2,6 +2,14 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
- var groupAnagrams = function(strs) {
-
+var groupAnagrams = function (strs) {
+    var m = {};
+    for (var item of strs) {
+        const key = item.split('').sort().join('');
+        if (!m.hasOwnProperty(key)) {
+            m[key] = [];
+        }
+        m[key].push(item);
+    }
+    return Object.keys(m).map(k => m[k]);
 };
