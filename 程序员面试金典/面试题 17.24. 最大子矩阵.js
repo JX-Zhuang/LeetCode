d2c9dab1755ca3f1a3de60806af77f808f9d7324
JX-Zhuang <who.zhuang@gmail.com>
2021-11-18 23:12:21 +0800

    getMaxMatrix

--- 程序员面试金典/面试题 17.24. 最大子矩阵.js
+++ 程序员面试金典/面试题 17.24. 最大子矩阵.js
@@ -2,6 +2,25 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
- var getMaxMatrix = function(matrix) {
-
+var getMaxMatrix = function (matrix) {
+    var ans = [], m = matrix.length, n = matrix[0].length, sum, max = Number.MIN_SAFE_INTEGER;
+    for (var i = 0; i < m; i++) {
+        sum = new Array(n).fill(0);
+        for (var j = i; j < m; j++) {
+            var start = 0, dp = 0;
+            for (var k = 0; k < n; k++) {
+                sum[k] += matrix[j][k];
+                dp += sum[k];
+                if (dp > max) {
+                    ans = [i, start, j, k];
+                    max = dp;
+                }
+                if (dp < 0) {
+                    dp = 0;
+                    start = k + 1;
+                }
+            }
+        }
+    }
+    return ans;
 };
