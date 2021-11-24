851fe4b45c4a9158818a328da672a8c675c8c2c9
JX-Zhuang <who.zhuang@gmail.com>
2021-11-24 23:57:31 +0800

    MyCircularDeque

--- 队列/641. 设计循环双端队列.js
+++ 队列/641. 设计循环双端队列.js
@@ -1,66 +1,81 @@
 /**
  * @param {number} k
  */
-var MyCircularDeque = function(k) {
-
+var MyCircularDeque = function (k) {
+    this.capacity = k + 1;
+    this.front = 0; //第一位有效的位置
+    this.rear = 0;  //队尾位置的下一位
+    this.q = new Array(this.capacity);
 };
 
 /** 
  * @param {number} value
  * @return {boolean}
  */
-MyCircularDeque.prototype.insertFront = function(value) {
-
+MyCircularDeque.prototype.insertFront = function (value) {
+    if (this.isFull()) return false;
+    this.front = (this.front - 1 + this.capacity) % this.capacity;
+    this.q[this.front] = value;
+    return true;
 };
 
 /** 
  * @param {number} value
  * @return {boolean}
  */
-MyCircularDeque.prototype.insertLast = function(value) {
-
+MyCircularDeque.prototype.insertLast = function (value) {
+    if (this.isFull()) return false;
+    this.q[this.rear] = value;
+    this.rear = (this.rear + 1) % this.capacity;
+    return true;
 };
 
 /**
  * @return {boolean}
  */
-MyCircularDeque.prototype.deleteFront = function() {
-
+MyCircularDeque.prototype.deleteFront = function () {
+    if (this.isEmpty()) return false;
+    this.front = (this.front + 1) % this.capacity;
+    return true;
 };
 
 /**
  * @return {boolean}
  */
-MyCircularDeque.prototype.deleteLast = function() {
-
+MyCircularDeque.prototype.deleteLast = function () {
+    if (this.isEmpty()) return false;
+    this.rear = (this.rear - 1 + this.capacity) % this.capacity;
+    return true;
 };
 
 /**
  * @return {number}
  */
-MyCircularDeque.prototype.getFront = function() {
-
+MyCircularDeque.prototype.getFront = function () {
+    if (this.isEmpty()) return -1;
+    return this.q[this.front];
 };
 
 /**
  * @return {number}
  */
-MyCircularDeque.prototype.getRear = function() {
-
+MyCircularDeque.prototype.getRear = function () {
+    if (this.isEmpty()) return -1;
+    return this.q[(this.rear - 1 + this.capacity) % this.capacity];
 };
 
 /**
  * @return {boolean}
  */
-MyCircularDeque.prototype.isEmpty = function() {
-
+MyCircularDeque.prototype.isEmpty = function () {
+    return this.front === this.rear;
 };
 
 /**
  * @return {boolean}
  */
-MyCircularDeque.prototype.isFull = function() {
-
+MyCircularDeque.prototype.isFull = function () {
+    return this.front === (this.rear + 1) % this.capacity;
 };
 
 /**
