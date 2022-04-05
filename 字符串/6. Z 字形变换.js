f51808add32b70e0e3204adf91e29d629ecd8703
JX-Zhuang <who.zhuang@gmail.com>
2022-04-06 00:42:37 +0800

    Z

---
+++ 字符串/6. Z 字形变换.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+    if (numRows <= 1) return s;
+    var arr = new Array(numRows).fill('');
+    var row = 0, flag = -1;
+    for (var i = 0; i < s.length; i++) {
+        arr[row] += s[i];
+        if (row === 0 || row === numRows - 1) {
+            flag = -flag;
+        }
+        row += flag;
+    }
+    return arr.join('');
+};
