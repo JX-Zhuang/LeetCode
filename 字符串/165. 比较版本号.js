151653e43231da4bba583cf0d96adccfed4cbdaf
JX-Zhuang <who.zhuang@gmail.com>
2022-05-05 18:14:20 +0800

    compareVersion

--- 字符串/165. 比较版本号.js
+++ 字符串/165. 比较版本号.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var compareVersion = function (version1, version2) {
-    var v1 = version1.split('.').map(i => Number(i)),
-        v2 = version2.split('.').map(i => Number(i));
-    if (v1.length > v2.length) {
-        while (v1.length !== v2.length)
-            v2.push(0);
-    } else if (v1.length < v2.length) {
-        while (v1.length !== v2.length)
-            v1.push(0);
-    }
-    for (var i = 0; i < v1.length; i++) {
-        if (v1[i] > v2[i]) return 1;
-        if (v1[i] < v2[i]) return -1;
+    var v1 = version1.split('.'),
+        v2 = version2.split('.');
+    for (var i = 0; i < v1.length || i < v2.length; i++) {
+        let x = 0; y = 0;
+        if (i < v1.length) {
+            x = Number(v1[i]);
+        }
+        if (i < v2.length) {
+            y = Number(v2[i]);
+        }
+        if (x > y) return 1;
+        if (x < y) return -1;
     }
     return 0;
 };
