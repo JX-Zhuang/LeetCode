083fc7a849f123ff2597e9244791492844ee3ab1
JX-Zhuang <who.zhuang@gmail.com>
2022-05-11 16:59:07 +0800

    getIntersectionNode

---
+++ 链表/160. 相交链表.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    var a = headA, b = headB;
+    while (a !== b) {
+        a = a ? a.next : headB;
+        b = b ? b.next : headA;
+    }
+    return b;
+};
