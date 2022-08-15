e9c9db776ac1ea85e07c444ca4dd5955558fd2e3
JX-Zhuang <who.zhuang@gmail.com>
2022-08-15 17:10:31 +0800

    update

--- Sliding Window/3. 无重复字符的最长子串.js
+++ Sliding Window/3. 无重复字符的最长子串.js
@@ -1,22 +1,32 @@
 /**
+ * 示例 1:
+
+输入: s = "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    var window = {};
-    var left = 0, right = 0;
-    var res = 0;
-    while (right < s.length) {
-        var c = s[right++];
-        if (!window.hasOwnProperty(c)) {
-            window[c] = 0;
+    var left = 0, max = 0, windows = {};
+    for (var i = 0; i < s.length; i++) {
+        var item = s[i];
+        if (windows.hasOwnProperty(item)) {
+            left = Math.max(left, windows[item] + 1);
         }
-        window[c]++;
-        while (window[c] > 1) {
-            var d = s[left++];
-            window[d]--;
-        }
-        res = Math.max(res, right - left);
+        max = Math.max(max, i - left + 1);
+        windows[item] = i;
     }
-    return res;
+    return max;
 };
